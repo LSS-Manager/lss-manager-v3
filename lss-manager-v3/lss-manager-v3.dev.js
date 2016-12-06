2f12438001572814c6ed7434e1a0987acf7b0cfc
wellá <weller.andrethomas@web.de>
2016-12-06 22:36:08 +0100

    initial setup for autoupdate file

---
+++ lss-manager-v3/lss-manager-v3.dev.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         LSS Manager v3 AutoUpdate
+// @version      3.0
+// @description  Das Tool für LSS
+// @author       lost | mausmajor | chaoskai | JanH8 | in memories: northdegree
+// @include      *://www.leitstellenspiel.de/
+// @include      *://www.leitstellenspiel.de/*
+// @include      *://www.missionchief.com/*
+// @include      *://www.missionchief.com/
+// @grant        none
+// @run          document-start
+// ==/UserScript==
