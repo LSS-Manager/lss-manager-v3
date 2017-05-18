bb66ddd0f460fa82fac58cbb965fdaeea5764e79
wellá <weller.andrethomas@web.de>
2017-05-18 20:33:18 +0200

    mega update
    war mal wieder zu faul mit git zu arbeiten, soz

---
+++ sources/lss-keyboardAlert/lss-keyboardAlert.js
@@ -0,0 +1,52 @@
+$(document).keydown(function (e) {
+    if (!($("input").is(":focus"))) {
+        switch (e.keyCode) {
+            case 68:
+                $("#mission_next_mission_btn")[0].click();
+                break;
+            case 87:
+                $("#mission_alliance_share_btn")[0].click();
+                break;
+            case 65:
+                $("#mission_previous_mission_btn")[0].click();
+                break;
+            case 83:
+                $(".alert_next").first().click();
+                break;
+            case 81:
+                $('a:contains("Sprechwunsch bearbeiten")')[0].click();
+                break;
+            case 49:
+                $('a[id^="btn_approach_"]')[0].click();
+                break;
+            case 50:
+                $('a[id^="btn_approach_"]')[1].click();
+                break;
+            case 51:
+                $('a[id^="btn_approach_"]')[2].click();
+                break;
+            case 52:
+                $('a[id^="btn_approach_"]')[3].click();
+                break;
+            case 53:
+                $('a[id^="btn_approach_"]')[4].click();
+                break;
+            case 82:
+                $("#btn_back_to_mission").click();
+                break;
+            case 88:
+                $("#mission_alarm_btn").click();
+                break;
+            case 89:
+                $("#mission_list").find("a[href^='/missions/']:eq(0)").click();
+                break;
+            case 70:
+                tellParent($('#lssm_searchEntryToggle').click());
+                break;
+            case 27:
+                tellParent(lightboxClose());
+                break;
+        }
+        return e.returnValue;
+    }
+});
