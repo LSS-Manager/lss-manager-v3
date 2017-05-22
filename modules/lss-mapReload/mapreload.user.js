2f4fb70519e7d8aa06279bd633f5f7a2185def7d
wellá <weller.andrethomas@web.de>
2017-05-23 00:26:28 +0200

    button style

--- modules/lss-mapReload/mapreload.user.js
+++ modules/lss-mapReload/mapreload.user.js
@@ -1,4 +1,4 @@
 (function() {
-    var clockDiv = $('<a class="btn btn-xs btn-default glyphicon glyphicon-refresh" style="position: relative;z-index: 9998;float: left;margin-left:10px;margin-top: 100px;" onclick="map.invalidateSize(true);"></a>');
+    var clockDiv = $('<a class="leaflet-bar leaflet-control leaflet-control-custom map-expand-button hidden-xs" style="height: 26px;width: 26px;position: relative;z-index: 9998;float: left;margin-left: 10px;margin-top: 110px;outline: none;background-color: white;" onclick="map.invalidateSize(true);"><i class="glyphicon glyphicon-refresh" style="font-size: 15px;padding: 5px;"></i></a>');
     $('#map').prepend(clockDiv);
 })();
