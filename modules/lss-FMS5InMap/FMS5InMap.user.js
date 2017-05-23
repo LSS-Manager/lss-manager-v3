8c7f7bb783ac115cd394d7d46fee771c5b86ca68
wellá <weller.andrethomas@web.de>
2017-05-23 20:39:49 +0200

    hopefully some fixes

--- modules/lss-FMS5InMap/FMS5InMap.user.js
+++ modules/lss-FMS5InMap/FMS5InMap.user.js
@@ -6,6 +6,13 @@
         opacity: ".7",
         "margin-top": "55px",
         display: "block",
-        "list-style-type": "none"
+        "list-style-type": "none",
+        left: "10px  !important",
+        bottom:"10px  !important",
+        width: "50%",
+        color: "rgba(0, 0, 0, 0)",
+        border: "none !important",
+        margin: "0  !important",
+        padding: "0  !important"
     })
 })();
