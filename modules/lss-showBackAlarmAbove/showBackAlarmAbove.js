cd98d5c754c583a33e70bc099d76d471c19dc36a
Mausmajor <Tobias@Tobias>
2017-05-26 00:14:47 +0200

    bugfix bei geteilten Missions Signed-off-by:Mausmajor <Tobias@Tobias>

--- modules/lss-showBackAlarmAbove/showBackAlarmAbove.js
+++ modules/lss-showBackAlarmAbove/showBackAlarmAbove.js
@@ -1,3 +1,7 @@
 (function ($) {
-    $('a[href$=backalarmAll]').length && $('#mission-aao-group').after($('a[href$=backalarmAll]').parent().clone());
+    if ($('#h2_vehicle_driving').length) {
+        $('a[href$=backalarmAll]').length && $('#h2_vehicle_driving').prev().after($('a[href$=backalarmAll]').parent().clone());
+    } else if ($('#h2_vehicles_at_mission').length) {
+        $('a[href$=backalarmAll]').length && $('#h2_vehicles_at_mission').prev().after($('a[href$=backalarmAll]').parent().clone());
+    }
 })($);
