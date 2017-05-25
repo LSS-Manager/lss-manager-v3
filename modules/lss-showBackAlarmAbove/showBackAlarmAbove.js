7adcb4da9bb1d0fedef86ea6cd34bc3acf5c7e18
Mausmajor <Tobias@Tobias>
2017-05-26 00:17:50 +0200

    bugfix bei geteilten Missions Signed-off-by:Mausmajor <Tobias@Tobias>

--- modules/lss-showBackAlarmAbove/showBackAlarmAbove.js
+++ modules/lss-showBackAlarmAbove/showBackAlarmAbove.js
@@ -1,7 +1,7 @@
 (function ($) {
     if ($('#h2_vehicle_driving').length) {
-        $('a[href$=backalarmAll]').length && $('#h2_vehicle_driving').prev().after($('a[href$=backalarmAll]').parent().clone());
+        $('a[href$=backalarmAll]').length && $('#h2_vehicle_driving').prev().after($('a[href$=backalarmAll]').parent().prev().clone()('a[href$=backalarmAll]').parent().clone());
     } else if ($('#h2_vehicles_at_mission').length) {
-        $('a[href$=backalarmAll]').length && $('#h2_vehicles_at_mission').prev().after($('a[href$=backalarmAll]').parent().clone());
+        $('a[href$=backalarmAll]').length && $('#h2_vehicles_at_mission').prev().after($('a[href$=backalarmAll]').parent().prev().clone()('a[href$=backalarmAll]').parent().clone());
     }
 })($);
