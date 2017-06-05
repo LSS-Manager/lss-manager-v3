65d8ba1da46af9ed0540f2eb0bcdf03cbb52184f
wellá <weller.andrethomas@web.de>
2017-06-05 19:34:41 +0200

    LOL WTF

--- modules/lss-redesign-01/redesign-01.user.js
+++ modules/lss-redesign-01/redesign-01.user.js
@@ -1,7 +1,7 @@
 (function () {
   $('head').prepend('<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" type="text/css" rel="stylesheet"/>');
   $('head').prepend('<link href="https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.0.3/leaflet.css" type="text/css" rel="stylesheet"/>');
-  $('.missionSideBarEntry > div > div > div > .col-xs-1 > img').detach().insertBefore('.panel-heading');
+  //$('.missionSideBarEntry > div > div > div > .col-xs-1 > img').detach().insertBefore('.panel-heading');
   $('body').prepend(
     '<style type="text/css" id="lss-redesign-01">'+
       'img.vehicle_search, .map_position_mover small, #arrow_build_feuerwache, #arrow_location_select, .glyphicon-user, .glyphicon-asterisk {'+
