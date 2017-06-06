6367110ecbe543687c17a73c31ad33872e9e5c47
wellá <weller.andrethomas@web.de>
2017-06-06 21:58:30 +0200

    layout02
    smol ui fixes

--- modules/lss-layout-02/layout-02.user.js
+++ modules/lss-layout-02/layout-02.user.js
@@ -2,11 +2,11 @@
 	$('body').prepend(
 		'<style type="text/css" id="lss-layout-02-style">'+
 		'.row{margin-top:0!important}'+
-		'#map_outer{padding:0;margin-bottom:15px; width:100% !important;}'+
-		'#missions_outer{padding-left:0!important}'+
+		'#map_outer{padding: 0;margin-bottom: 15px;margin-left: 15px;margin-right: 15px;width: calc(100% - 30px) !important;;}'+
+		'#missions_outer{padding-left:15!important}'+
 		'#buildings_outer{padding-right:7.5px!important;padding-left:0!important}'+
 		'#chat_outer{padding-left:7.5px!important;padding-right:0!important}'+
-		'#radio_outer{padding-right:0}'+
+		'#radio_outer{padding-right:0; width: calc(25% - 30px) !important;}'+
 		'#map{height:400px}'+
 		'#missions-panel-body{padding:0;height:500px!important}'+
 		'#missions > div > div > strong{display:none}'+
