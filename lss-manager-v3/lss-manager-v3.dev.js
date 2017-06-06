8975241d821c9b990ad6371a96ebf3924accbfcb
Tsumiki-Chan <mirkoiz@hotmail.de>
2017-06-06 23:24:10 +0200

    [ADDED] Event hooks

--- lss-manager-v3/lss-manager-v3.dev.js
+++ lss-manager-v3/lss-manager-v3.dev.js
@@ -989,6 +989,36 @@ var module = {
     }
 };
 
+var lssm_hook = {
+    orgfunctions: {},
+    prename: function(event, func){
+        "use strict";
+        var self = this;
+        if(!this.orgfunctions.hasOwnProperty(event)){
+            this.orgfunctions[event] = window[event];
+            window[event] = function(){
+                $(document).trigger("lssm_"+event+"_before",arguments);
+                self.orgfunctions[event].apply(window, arguments);
+                $(document).trigger("lssm_"+event+"_after",arguments);
+            }
+        }
+        return "lssm_"+event+"_before";
+    },
+    postname: function(event, func){
+        "use strict";
+        var self = this;
+        if(!this.orgfunctions.hasOwnProperty(event)){
+            this.orgfunctions[event] = window[event];
+            window[event] = function(){
+                $(document).trigger("lssm_"+event+"_before",arguments);
+                self.orgfunctions[event].apply(window, arguments);
+                $(document).trigger("lssm_"+event+"_after",arguments);
+            }
+        }
+        return "lssm_"+event+"_after";
+    }
+};
+
 
 (function (I18n, $) {
     $("head").prepend('<link href="' + lssm.getlink('/lss-manager-v3/css/main.css') +'" rel="stylesheet" type="text/css">');
@@ -1025,7 +1055,6 @@ var module = {
                 if (lssm.Module[i].active == false)
                     lssm.Module[i].active = modules[i];
             }
-
             module.loadall();
             appstore.appendAppstore();
         }
