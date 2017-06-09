48c54f24760e83994fd433cd9cea394cc5c4138f
tolit00 <toflit00@hs-esslingen.de>
2017-06-09 21:41:03 +0200

    an sebastians neuerung angepasst Signed-off-by:tolit00 <toflit00@hs-esslingen.de>

--- modules/lss-RenameFZ/renameFZ.user.js
+++ modules/lss-RenameFZ/renameFZ.user.js
@@ -1,5 +1,5 @@
 (function (I18n, $) {
-    if (!$('#tab_vehicle').length || !$('#iframe-inside-container  img[alt="Building_leitstelle"]').length)
+    if (!document.URL.match(/buildings/) || !$('#iframe-inside-container > h1[building_type="7"]'))
         return;
     $('#tab_vehicle').on('DOMNodeInserted', 'script', createSettings);
     I18n.translations.de['lssm']['renameFZ'] = {
