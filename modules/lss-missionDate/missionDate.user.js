990ef3bc53131904d5da650af1be069c01860704
MrWeezle <redshadow-simon@web.de>
2017-06-20 11:29:53 +0200

    no message

--- modules/lss-missionDate/missionDate.user.js
+++ modules/lss-missionDate/missionDate.user.js
@@ -13,52 +13,78 @@
     };
 	
     var h1 = document.getElementById('missionH1');
-    var einsatzdate = h1.getAttribute("data-original-title");
-	var a;
-	if(I18n.locale == 'de')
-	{
-		h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+' <span id="einsatzdate"></span></small><br>');
-		a = einsatzdate.replace(/[a-zA-Z]/ig,'').replace(': ','').replace('.','').replace(' , ',':').trim().split(':');
+	if (h1 !== null)
+    {
+		var einsatzdate = h1.getAttribute("data-original-title");
+		var a;
+		if(I18n.locale == 'de')
+		{
+			h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+' <span id="einsatzdate"></span></small><br>');
+			a = einsatzdate.replace(/[a-zA-Z]/ig,'').replace(': ','').replace('.','').replace(' , ',':').trim().split(':');
+		}
+		else
+		{
+			h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span></small> '+I18n.t('lssm.missionDate.ago')+'<br>');
+			a = einsatzdate.replace(/[a-zA-Z]/ig,'').replace(': ','').replace('  ',':').trim().split(':');
+		}
+
+		var oldDay = a[0];
+		var oldHour = a[1];
+		var oldMin = a[2];
+
+		var strcount;
+		var x = new Date();
+		var day = x.getDate();
+		var hour = x.getHours();
+		var min = x.getMinutes();
+		var newDay, newHour, newMin;
+
+		if(oldDay > day)
+		{
+			if(hour > oldHour)
+			{
+				if(oldDay == 31)
+					newDay = day;
+				if(oldDay == 30 && day >= 1)
+					newDay = day+1;
+			}
+		}
+		else {
+			if(oldHour > hour)
+			{
+				if(oldMin > min)
+					newDay = (day-oldDay)-1;
+			}
+			newDay = day-oldDay;
+		}
+		if (oldHour > hour) {
+			newHour = (24-oldHour)+hour;
+		}
+		else {
+			newHour = hour-oldHour;
+		}
+
+		if (oldMin > min) {
+			newMin = (60 - oldMin) + min;
+			newHour--;
+		}
+		else {
+			newMin = min-oldMin;
+		}
+
+		if (newDay >= 1)
+			newDay = newDay + 'd ';
+		else
+			newDay = '';
+		
+		if (newHour >= 1)
+			newHour = newHour + 'h ';
+		else
+			newHour = '';
+
+		if (newMin < 0)
+			newMin = 0;
+		if (document.getElementById('einsatzdate') !== null)
+			document.getElementById('einsatzdate').innerHTML = newDay + newHour + newMin + ' min';
 	}
-	else
-	{	
-		h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span></small>'+I18n.t('lssm.missionDate.ago')+'<br>');
-		a = einsatzdate.replace(/[a-zA-Z]/ig,'').replace(': ','').replace('  ',':').trim().split(':');
-	}	
-
-    
-    var oldHour = a[1];
-    var oldMin = a[2];
-
-    var strcount;
-    var x = new Date();
-    var hour = x.getHours();
-    var min = x.getMinutes();
-    var newHour, newMin;
-
-    if (oldHour > hour) {
-        newHour = (24-oldHour)+hour;
-    }
-    else {
-        newHour = hour-oldHour;
-    }
-
-    if (oldMin > min) {
-        newMin = (60 - oldMin) + min;
-        newHour--;
-    }
-    else {
-        newMin = min-oldMin;
-    }
-
-    if (newHour >= 1)
-        newHour = newHour + 'h ';
-    else
-        newHour = '';
-
-    if (newMin < 0)
-        newMin = 0;
-    if (document.getElementById('einsatzdate') !== null)
-        document.getElementById('einsatzdate').innerHTML = newHour + newMin + ' min';
-    
 })();
