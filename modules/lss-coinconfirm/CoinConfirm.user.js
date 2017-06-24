bda4e8e51b2bffc1fa82524d82a85a6421b80f8e
Jalibu <dev@localhost>
2017-06-24 23:02:28 +0200

    Bugfix CoinConfirm

--- modules/lss-coinconfirm/CoinConfirm.user.js
+++ modules/lss-coinconfirm/CoinConfirm.user.js
@@ -6,13 +6,21 @@
 	};
 
 	I18n.translations.en['lssm']['coinconfirm'] = {
-		message : "Möchtest du wirklich mit Coins bezahlen?"
+		message : "Please confirm the payment with coins."
 	};
 
 	function protectCoins() {
-		console.log("hi");
+		
+		// Gebäude bauen
+		$(document).ajaxComplete(function() {
+			 $('.coins_activate').attr('onclick', 'return confirm("' + I18n.t('lssm.coinconfirm.message') + '")');
+		});
+		
+		// Ausbau starten, Personal einstellen
 		$('a[href*="coins"]').attr('onclick', 'return confirm("' + I18n.t('lssm.coinconfirm.message') + '")');
 		$('a[href*="Coins"]').attr('onclick', 'return confirm("' + I18n.t('lssm.coinconfirm.message') + '")');
+		// Ausbau fertigstellen
+		$('a[href*="extension_finish"]').attr('onclick', 'return confirm("' + I18n.t('lssm.coinconfirm.message') + '")');
 	}
 	
 	protectCoins();
