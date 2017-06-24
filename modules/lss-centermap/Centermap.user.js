9ef0ed4edeb46954ef89a6afa19b35a691da6087
Jalibu <dev@localhost>
2017-06-24 11:33:24 +0200

    Centermap regards stations for zoom level now

--- modules/lss-centermap/Centermap.user.js
+++ modules/lss-centermap/Centermap.user.js
@@ -21,6 +21,14 @@
     	    if(position.lng < lng_min || lng_min === undefined) lng_min = position.lng;
     	    if(position.lng > lng_max || lng_max === undefined) lng_max = position.lng;
     	});
+    	
+		$(lssm.get_buildings()).each(function(){
+    	    if(this.stationLat < lat_min || lat_min === undefined) lat_min = this.stationLat;
+    	    if(this.stationLat > lat_max || lat_max === undefined) lat_max = this.stationLat;
+    	    if(this.stationLng < lng_min || lng_min === undefined) lng_min = this.stationLng;
+    	    if(this.stationLng > lng_max || lng_max === undefined) lng_max = this.stationLng;
+		});
+    	
     	map.fitBounds([[lat_min, lng_min],[lat_max, lng_max]]);
     }
     
