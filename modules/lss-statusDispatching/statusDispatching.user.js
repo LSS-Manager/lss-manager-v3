08fb286c9ecc40edd7f5b2a0961144c649d3ddb2
Simon W <MrWeezle@users.noreply.github.com>
2017-06-28 17:25:30 +0200

    WIP | #155
    Waiting for Missing EN + NL translations

--- modules/lss-statusDispatching/statusDispatching.user.js
+++ modules/lss-statusDispatching/statusDispatching.user.js
@@ -10,7 +10,8 @@
 	settingsAttr: '"Sprechwunsch bearbeiten" in Einsatzmaske automatisch anklicken',
 	settingsAttrDesc: 'Die Aktivierung dieser Option bewirkt, dass bei einem Einsatz mit einem offenen Status 5 zuerst der Status bearbeitet werden muss, bevor der Einsatz weiter bearbeitet werden kann. Wird diese Option also aktiviert, werden alle Status 5 schneller bearbeitet.<br><br>'+
 						'Wird die Option deaktiviert, werden nur noch die Buttons "Zum nächsten Fahrzeug im Status 5" und "Zurück zum Einsatz" automatisch angeklickt.',
-	close:'Schließen'
+	close:'Schließen',
+	fms5H1: 'transportiert'
     };
 
     I18n.translations.en['lssm']['statusDispatching'] = {
@@ -22,7 +23,8 @@
 	settingsAttr: 'Click the button "Transport Requested" in mission mask automatically',
 	settingsAttrDesc: 'Enabling this option provides automatic clicking on the "transport requested" button in the mission mask. This way you have to go through all transport requests before you can view details to the mission. Enabling this option allows faster processing of the transport requests.<br><br>'+
 	'Disabling this option allows to see mission details before processing the transport requests. The buttons "Go to the next vehicle with a transport request" and "Back to mission" are not affected by this setting and are always automatically clicked.',
-	close:'Close'
+	close:'Close',
+	fms5H1: 'transportiert'
     };
 
     I18n.translations.nl['lssm']['statusDispatching'] = {
@@ -34,7 +36,8 @@
 	settingsAttr: 'De knop "Spraakaanvraag beantwoorden" in het meldingsvenster automatisch indrukken.', //TODO: NL
 	settingsAttrDesc: 'Door deze optie in te schakelen wordt de knop "spraakaanvraag beantwoorden" in het meldingsvenster automatisch ingedrukt. Op deze manier verwerk je sneller achter elkaar alle spraakaanvragen in plaats van dat je eerst nog de details van de desbetreffende melding te zien krijgt.<br><br>'+
 	'Uitgeschakeld krijg je wel nog alle meldingen apart te zien voordat je de spraakaanvraag beantwoordt. De knoppen "Naar het volgende status 7 voertuig" en "Naar het incident" worden niet door deze instelling beïnvloed.', //TODO: NL
-	close:'Dicht'
+	close:'Dicht',
+	fms5H1: 'transportiert'
     };
 	
 	function getSettings(){
@@ -74,6 +77,9 @@
 
 	function statusDispatching()
 	{
+		var backMission_clicked = false;
+		var next_clicked = false;
+		
 		if(getSetting('fastS5'))
 		{
 			var fms = document.getElementsByClassName('btn btn-xs btn-success');
@@ -83,19 +89,35 @@
 			}
 		}
 		var nextVehicle = document.getElementsByClassName('btn btn-success');
-		var next_clicked = false;
+		var count = 0;
 		for (var i = 0;i<nextVehicle.length;i++)
 		{
 			if (nextVehicle.length > 0 && nextVehicle[i].innerText.match(I18n.t('lssm.statusDispatching.nextVehicle'))) {
 				nextVehicle[i].click();
 				next_clicked = true;
 			}
+			else{
+				count++;
+			}
 		}
 		
 		var backToMission = document.getElementById('btn_back_to_mission');
-		
 		if (backToMission !== null && !next_clicked) {
 			backToMission.click();
+			backMission_clicked = true;
+		}
+		
+		var label = document.getElementsByClassName('alert alert-success');
+		allTransported = false;
+		for (var i = 0;i<label.length;i++)
+		{
+			if (label[i].innerText.match(I18n.t('lssm.statusDispatching.fms5H1'))) {
+				allTransported = true;
+			}
+		}
+		if(backToMission === null && count == nextVehicle.length && allTransported) 
+		{
+			tellParent("lightboxClose();");
 		}
 	}
 		
