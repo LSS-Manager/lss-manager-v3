6dfe8330e836b3be3b8d5dbc5dc92168822ad1ea
Jalibu <dev@localhost>
2017-06-30 22:01:46 +0200

    Remove Sonar Code Smells

--- modules/lss-centermap/Centermap.user.js
+++ modules/lss-centermap/Centermap.user.js
@@ -2,7 +2,7 @@
 	'use strict';
 
 	var VERSION = "2.1";
-	
+
 	var LSS_CENTERMAP_STORAGE = "LSS_CENTERMAP_STORAGE";
 
 	I18n.translations.de['lssm']['centermap'] = {
@@ -36,90 +36,100 @@
 			set : "Save current position"
 		}
 	};
-	
+
 	var managedSettings = {
-		    "id": LSS_CENTERMAP_STORAGE,
-		    "title": I18n.t('lssm.centermap.settings.title'),
-		    "version": VERSION,
-		    "settings": {
-		        "centermap-option": {
-		            "default": "2",
-		            "ui": {
-		                "label": I18n.t('lssm.centermap.settings.choose'),
-		                "type": "radio",
-		                "options": [
-		                	{	"title": I18n.t('lssm.centermap.settings.option1'),
-		                		"value": "1",
-		                		"description" : I18n.t('lssm.centermap.settings.option1desc')
-		                	},
-		                	{	"title": I18n.t('lssm.centermap.settings.option2'),
-		                		"value": "2",
-		                		"description" : I18n.t('lssm.centermap.settings.option2desc')
-		                	},
-		                	{	"title": I18n.t('lssm.centermap.settings.option3'),
-		                		"value": "3",
-		                		"description" : I18n.t('lssm.centermap.settings.option3desc')
-		                	}
-		            	]
-		            }
-		        },
-		        "centermap-alliance": {
-		            "default": false,
-		            "ui": {
-		                "label": I18n.t('lssm.centermap.settings.alliance'),
-		                "type": "boolean",
-		                "parent": "LSS_CENTERMAP_STORAGE_centermap-option_1"
-		            }
-		        },
-		        "centermap-center-lat": {
-		            "default": "",
-		            "ui": {
-		                "label": "",
-		                "type": "hidden"
-		            }
-		        },
-		        "centermap-center-lng": {
-		            "default": "",
-		            "ui": {
-		                "label": "",
-		                "type": "hidden"
-		            }
-		        },
-		        "centermap-zoom": {
-		            "default": "",
-		            "ui": {
-		                "label": "",
-		                "type": "hidden"
-		            }
-		        },
-		        "centermap-button": {
-		            "default": "",
-		            "ui": {
-                        "label": I18n.t('lssm.centermap.settings.set'),
-                        "type": "button",
-                        "custom_function_event": "click",
-                        "custom_function": function() {
-                            var prefix = "LSS_CENTERMAP_STORAGE_";
-                            $('#' + prefix + 'centermap-center-lat').val(map.getCenter().lat);
-                            $('#' + prefix + 'centermap-center-lng').val(map.getCenter().lng);
-                            $('#' + prefix + 'centermap-zoom').val(map.getZoom());
-                        },
-                        "parent": "LSS_CENTERMAP_STORAGE_centermap-option_2"
-                    }
-		        }
-		    }
-		};
-	
+		"id" : LSS_CENTERMAP_STORAGE,
+		"title" : I18n.t('lssm.centermap.settings.title'),
+		"version" : VERSION,
+		"settings" : {
+			"centermap-option" : {
+				"default" : "2",
+				"ui" : {
+					"label" : I18n.t('lssm.centermap.settings.choose'),
+					"type" : "radio",
+					"options" : [
+							{
+								"title" : I18n
+										.t('lssm.centermap.settings.option1'),
+								"value" : "1",
+								"description" : I18n
+										.t('lssm.centermap.settings.option1desc')
+							},
+							{
+								"title" : I18n
+										.t('lssm.centermap.settings.option2'),
+								"value" : "2",
+								"description" : I18n
+										.t('lssm.centermap.settings.option2desc')
+							},
+							{
+								"title" : I18n
+										.t('lssm.centermap.settings.option3'),
+								"value" : "3",
+								"description" : I18n
+										.t('lssm.centermap.settings.option3desc')
+							} ]
+				}
+			},
+			"centermap-alliance" : {
+				"default" : false,
+				"ui" : {
+					"label" : I18n.t('lssm.centermap.settings.alliance'),
+					"type" : "boolean",
+					"parent" : "LSS_CENTERMAP_STORAGE_centermap-option_1"
+				}
+			},
+			"centermap-center-lat" : {
+				"default" : "",
+				"ui" : {
+					"label" : "",
+					"type" : "hidden"
+				}
+			},
+			"centermap-center-lng" : {
+				"default" : "",
+				"ui" : {
+					"label" : "",
+					"type" : "hidden"
+				}
+			},
+			"centermap-zoom" : {
+				"default" : "",
+				"ui" : {
+					"label" : "",
+					"type" : "hidden"
+				}
+			},
+			"centermap-button" : {
+				"default" : "",
+				"ui" : {
+					"label" : I18n.t('lssm.centermap.settings.set'),
+					"type" : "button",
+					"custom_function_event" : "click",
+					"custom_function" : function() {
+						var prefix = "LSS_CENTERMAP_STORAGE_";
+						$('#' + prefix + 'centermap-center-lat').val(
+								map.getCenter().lat);
+						$('#' + prefix + 'centermap-center-lng').val(
+								map.getCenter().lng);
+						$('#' + prefix + 'centermap-zoom').val(map.getZoom());
+					},
+					"parent" : "LSS_CENTERMAP_STORAGE_centermap-option_2"
+				}
+			}
+		}
+	};
+
 	lssm.managedSettings.register(managedSettings);
 
 	function centerMap() {
-		if(getSetting('centermap-option') == 2){
+		if (getSetting('centermap-option') === "2") {
 			var lat_min;
 			var lat_max;
 			var lng_min;
 			var lng_max;
 			$(mission_markers).each(function() {
-				if(getSetting('centermap-alliance') || this.involved){
+				if (getSetting('centermap-alliance') || this.involved) {
 					var position = this._latlng;
 					if (position.lat < lat_min || lat_min === undefined)
 						lat_min = position.lat;
@@ -144,17 +154,17 @@
 			});
 
 			map.fitBounds([ [ lat_min, lng_min ], [ lat_max, lng_max ] ]);
-		} else if(getSetting('centermap-option') == 3) {
-			map.setView([getSetting('centermap-center-lat'), getSetting('centermap-center-lng')], getSetting('centermap-zoom'));
+		} else if (getSetting('centermap-option') === "3") {
+			map.setView([ getSetting('centermap-center-lat'),
+					getSetting('centermap-center-lng') ],
+					getSetting('centermap-zoom'));
 		}
-		
-	}
-	
-	function getSetting(setting){
-		return lssm.managedSettings.getSetting(LSS_CENTERMAP_STORAGE, setting);
+
 	}
 
-	
+	function getSetting(setting) {
+		return lssm.managedSettings.getSetting(LSS_CENTERMAP_STORAGE, setting);
+	}
 
 	$(map).on('lssm-map-reload', function() {
 		centerMap();
