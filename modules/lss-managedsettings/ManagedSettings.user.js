ba4fddd3cece0e3baad2d51ed1f00ec4e2e7db43
jalibu <Jan.litzenburger@gmail.com>
2017-07-05 17:21:01 +0200

    LSSM-185 + LSSM-186: Fixed double opening Bug & Order modules alphabe… (#187)
    * LSSM-185 + LSSM-186: Fixed double opening Bug & Order modules alphabetical
    
    * LSSM-185 + LSSM-186: Fixed some sonar issues and checkbox bug

--- modules/lss-managedsettings/ManagedSettings.user.js
+++ modules/lss-managedsettings/ManagedSettings.user.js
@@ -25,6 +25,7 @@
 	};
 	
 	function renderSettings() {
+		if($('#' + lssm.config.prefix + '_appstore_ManagedSettings').length > 0) return false;
 		var markup = '<div class="jumbotron" id="' + lssm.config.prefix + '_appstore_ManagedSettings">';
 		markup += '<h1>' + I18n.t('lssm.managedsettings.title') + '</h1>';
 		markup += '<p>' + I18n.t('lssm.managedsettings.text1') + '</p>';
@@ -42,8 +43,20 @@
 		markup += '</div>';
 		$('#map_outer').before(markup);
 		
-		for(var moduleKey in lssm.managedSettings.registeredModules) {
-			var module = lssm.managedSettings.registeredModules[moduleKey];
+		var sortable = [];
+		for (var module in lssm.managedSettings.registeredModules) {
+		    sortable.push(lssm.managedSettings.registeredModules[module]);
+		}
+		
+		sortable.sort(function(a,b){
+		    if(a.title < b.title) return -1;
+		    if(a.title > b.title) return 1;
+		    return 0;
+		});
+		
+		$.each(sortable, function(){
+			var module = this;
+			var moduleKey = module.id;
 			markup = "";
 			markup += '<div id="' + moduleKey + '_wrap">';
 			markup += '<h3>' + module.title + '</h3>';
@@ -56,7 +69,7 @@
 					$('#' + moduleKey + '_wrap').append(renderUIElement(moduleKey, settingsKey, module.settings[settingsKey]));
 				}
 			}
-		};
+		});
 	
 
 		
@@ -72,10 +85,7 @@
             var module = lssm.managedSettings.registeredModules[moduleKey];
             for(var settingsKey in module.settings) {
                 var setting = module.settings[settingsKey];
-                console.log(setting.ui.custom_function);
-                console.log(setting.ui.custom_function_event);
                 if(setting.ui.custom_function && setting.ui.custom_function_event){
-                	console.log("huuu");
                     $('#' + moduleKey + '_' + settingsKey).on(setting.ui.custom_function_event,setting.ui.custom_function);    
                 }
             }
@@ -85,7 +95,7 @@
 	function renderUIElement(moduleKey, settingsKey, element){
 		var elementName = moduleKey + '_' + settingsKey;
 		var response = '<div id="' + elementName + '_wrap">';
-		if(element.ui.type == "radio"){
+		if(element.ui.type === "radio"){
 			var optionCount = 0;
 			$.each(element.ui.options, function(){
 				var prop_checked = "";
@@ -97,23 +107,23 @@
 				response += '</div>';
 				optionCount ++;
 			});
-		} else if(element.ui.type == "checkbox"){
-			var checked = element.value == true ? " checked " : "";
+		} else if(element.ui.type === "checkbox"){
+			var checked = element.value === true ? " checked " : "";
 			response += '<div style="margin-left: 16px;"><input type="checkbox" ' + checked + ' style="margin-right: 4px;" name="' + elementName + '" id="' + elementName + '">' + element.ui.label + '</div>';
-			response += '<div style="margin-left: 16px;">' + element.ui.description + '</div>';
-		} else if(element.ui.type == "hidden"){
+			if(element.ui.description) response += '<div style="margin-left: 16px;">' + element.ui.description + '</div>';
+		} else if(element.ui.type === "hidden"){
 			response += '<input type="hidden" value="' + element.value + '" id="' + elementName + '" name="' + elementName + '">';
-		} else if(element.ui.type == "button"){
+		} else if(element.ui.type === "button"){
 			response += '<button type="button" class="btn btn-grey btn-sm" id="' + elementName +'" style="margin-left: 16px;">';
 			response += '<span>' + element.ui.label + '</span>';
 			response += '</button>';
-		} else if(element.ui.type == "text" || element.ui.type == "int" || element.ui.type == "float"){
+		} else if(element.ui.type === "text" || element.ui.type == "int" || element.ui.type == "float"){
 			response += '<div id="' + elementName + '_wrap" ' + (element.ui.class ?  'class="' + element.ui.class + '"' : "") + '>';
 			response += '<label style="margin-left: 4px;" for="' + elementName + '">' + element.ui.label + '</label>';
 			response += '<input type="text" name="' + elementName + '" id="' + elementName + '" value="' + element.value + '">';
 			if(element.ui.description) response += '<div style="margin-left: 16px;">' + element.ui.description + '</div>';
 			response += '</div>';
-		} else if(element.ui.type == "toggle"){
+		} else if(element.ui.type === "toggle"){
 			response += '<div class="col-md-3"><div class="panel panel-default" style="display: inline-block;width:100%;" id="' + elementName + '_toggle_wrap">';
 			response += '<div class="panel-body"><span class="pull-right"><div class="onoffswitch">';
 			response += '<input class="onoffswitch-checkbox" '+ (element.value ? 'checked="true"':'')+' id="' + elementName + '" value="true" name="onoffswitch" type="checkbox">';
@@ -134,17 +144,17 @@
 				var setting = module.settings[settingsKey];
 				var elementName = moduleKey + '_' + settingsKey;
 				var formElement = $('#' + elementName);
-				if(setting.ui.type == 'checkbox' || setting.ui.type == 'toggle'){
+				if(setting.ui.type === 'checkbox' || setting.ui.type == 'toggle'){
 	                if (formElement.is(':checked')) {
 	                    setting.value = true;
 	                } else {
 	                    setting.value = false;
 	                }
-				} else if(setting.ui.type == 'radio'){
+				} else if(setting.ui.type === 'radio'){
 	                setting.value = $("input[name='" + elementName + "']:checked").val()
-	            } else if(setting.ui.type == 'int'){
+	            } else if(setting.ui.type === 'int'){
 	            	setting.value = parseInt(formElement.val());
-	            } else if(setting.ui.type == 'float'){
+	            } else if(setting.ui.type === 'float'){
 	            	setting.value = parseFloat(formElement.val());
 	            } else {
 	                setting.value = formElement.val();
