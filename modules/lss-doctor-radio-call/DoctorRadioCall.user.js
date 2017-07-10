58d3672f6c882979bfd8f25e33ed4699b2df06db
Jalibu <dev@localhost>
2017-07-10 20:00:50 +0200

    Removed Whitespaces

--- modules/lss-doctor-radio-call/DoctorRadioCall.user.js
+++ modules/lss-doctor-radio-call/DoctorRadioCall.user.js
@@ -1,19 +1,19 @@
 (function(I18n, $) {
 	'use strict';
-	
-    I18n.translations.de.lssm.doctorradiocall = {
-            nef: "NEF"
-    };
-    I18n.translations.en.lssm.doctorradiocall = {
-            nef: "HEMS"
-    };
-    I18n.translations.nl.lssm.doctorradiocall = {
-            nef: "MMT-Auto"
-    };
+
+	I18n.translations.de.lssm.doctorradiocall = {
+		nef : "NEF"
+	};
+	I18n.translations.en.lssm.doctorradiocall = {
+		nef : "HEMS"
+	};
+	I18n.translations.nl.lssm.doctorradiocall = {
+		nef : "MMT-Auto"
+	};
 
 	// Hook-in Patient Marker function to retrieve event
 	var patientMarkerAddOrig = patientMarkerAdd;
-	
+
 	patientMarkerAdd = function(t) { // NOSONAR: Variable is declared globally in the main game.
 		if (t.missing_text && t.missing_text.indexOf(I18n.t('lssm.doctorradiocall.nef')) >= 0) {
 			radioMessage({
@@ -31,7 +31,7 @@
 			// Due we do not get a calling vehicle_id we have to remove the
 			// corresponding button manually.
 			var el = $('#radio_messages_important').find('a[href="/vehicles/"]');
-			el.parent().find('a').click(function(){
+			el.parent().find('a').click(function() {
 				$(this).parent().hide();
 			});
 			el.remove();
