52df6526416ce70c84f470c4087e1570807d1123
Simon W <MrWeezle@users.noreply.github.com>
2017-07-11 13:07:56 +0200

    Spacing Fix

--- modules/lss-missionDate/missionDate.user.js
+++ modules/lss-missionDate/missionDate.user.js
@@ -1,80 +1,80 @@
 (function() {
-	
-	I18n.translations.de['lssm']['missionDate'] = {
+    
+    I18n.translations.de['lssm']['missionDate'] = {
         ago: 'Vor',
-		created: 'Einsatz eingegangen: ',
-		time_postfix: ' Uhr'
+        created: 'Einsatz eingegangen: ',
+        time_postfix: ' Uhr'
     };
 
     I18n.translations.en['lssm']['missionDate'] = {
         ago: 'ago',
-		created: 'Created at: ',
-		time_postfix: ' Uhr'
+        created: 'Created at: ',
+        time_postfix: ' Uhr'
     };
-	
-	I18n.translations.nl['lssm']['missionDate'] = {
+    
+    I18n.translations.nl['lssm']['missionDate'] = {
         ago: 'geleden',
-		created: 'Inzet begonnen: ',
-		time_postfix: ' Uhr'
+        created: 'Inzet begonnen: ',
+        time_postfix: ' Uhr'
     };
-	
+    
     var h1 = document.getElementById('missionH1');
-	if (h1 !== null)
+    if (h1 !== null)
     {
-		var einsatzdate = $('#missionH1').data('original-title');
-		if(I18n.locale == 'de')
-		{
-			h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+' <span id="einsatzdate"></span></small><br>');
-		}
-		else
-		{
-			h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span> '+I18n.t('lssm.missionDate.ago')+'</small><br>');
+        var einsatzdate = $('#missionH1').data('original-title');
+        if(I18n.locale == 'de')
+        {
+            h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+' <span id="einsatzdate"></span></small><br>');
+        }
+        else
+        {
+            h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span> '+I18n.t('lssm.missionDate.ago')+'</small><br>');
             einsatzdate = einsatzdate.replace(/ Jan /i, ' Januar ').replace(/ Feb /i, ' Februar ').replace(/ Mar /i, ' März ').replace(/ Apr /i, ' April ').replace(/ May /i, ' Mai ').replace(/ Jun /i, ' Juni ').replace(/ Jul /i, ' Juli ').replace(/ Aug /i, ' August ').replace(/ Sep /i, ' September ').replace(/ Okt /i, ' Oktober ').replace(/ Nov /i, ' November').replace(/ Dec /i, ' Dezember');
-		}
-		
-		
-		var currDate = new Date();
-		var tempOlddate = einsatzdate.replace(',', ' '+currDate.getFullYear()+',').replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
-		missionDate = new Date(tempOlddate);
-	
-		var timeDiff = currDate.getTime() - missionDate.getTime();
-		
-		if(timeDiff < 0)
-		{
-			tempOlddate = h1.getAttribute("data-original-title").replace(',', ' '+currDate.getFullYear()-1+',').replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
-			missionDate = new Date(tempOlddate);
-			timeDiff = currDate.getTime() - xDate.getTime();
-		}
-		
-		var minutes = (timeDiff/1000)/60;
-		var hours = minutes/60;
-		var days = hours/24;
-						
-		var newDay = Math.floor(days);
-		var newHour = Math.floor(hours % 24);
-		var newMin = Math.floor(minutes % 60);
-		
-		if (newDay >= 1)
-			newDay = newDay + 'd ';
-		else
-			newDay = '';
-		
-	    if (I18n.locale == 'en')
-		{
-		    var offset = currDate.getTimezoneOffset()/60;
-		    //Zahl 4, weil EST UTC+4 ist
-		    var newOffset = 4 - offset;
-	    	    newHour -= newOffset;
-    	}
-	    
-		if (newHour >= 1)
-			newHour = newHour + 'h ';
-		else
-			newHour = '';
-	    
-		if (newMin < 0)
-			newMin = 0;
-		if (document.getElementById('einsatzdate') !== null)
-			document.getElementById('einsatzdate').innerHTML = newDay + newHour + newMin + ' min';
-	}
+        }
+        
+        
+        var currDate = new Date();
+        var tempOlddate = einsatzdate.replace(',', ' '+currDate.getFullYear()+',').replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
+        missionDate = new Date(tempOlddate);
+    
+        var timeDiff = currDate.getTime() - missionDate.getTime();
+        
+        if(timeDiff < 0)
+        {
+            tempOlddate = h1.getAttribute("data-original-title").replace(',', ' '+currDate.getFullYear()-1+',').replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
+            missionDate = new Date(tempOlddate);
+            timeDiff = currDate.getTime() - xDate.getTime();
+        }
+        
+        var minutes = (timeDiff/1000)/60;
+        var hours = minutes/60;
+        var days = hours/24;
+                        
+        var newDay = Math.floor(days);
+        var newHour = Math.floor(hours % 24);
+        var newMin = Math.floor(minutes % 60);
+        
+        if (newDay >= 1)
+            newDay = newDay + 'd ';
+        else
+            newDay = '';
+        
+        if (I18n.locale == 'en')
+        {
+            var offset = currDate.getTimezoneOffset()/60;
+            //Zahl 4, weil EST UTC+4 ist
+            var newOffset = 4 - offset;
+                newHour -= newOffset;
+        }
+        
+        if (newHour >= 1)
+            newHour = newHour + 'h ';
+        else
+            newHour = '';
+        
+        if (newMin < 0)
+            newMin = 0;
+        if (document.getElementById('einsatzdate') !== null)
+            document.getElementById('einsatzdate').innerHTML = newDay + newHour + newMin + ' min';
+    }
 })();
