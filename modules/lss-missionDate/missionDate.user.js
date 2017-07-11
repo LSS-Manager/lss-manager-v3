a3b361d9c5ac59661d80623c43aae6af87f0e5ab
Simon W <MrWeezle@users.noreply.github.com>
2017-07-11 15:09:33 +0200

    Sonar Fixes

--- modules/lss-missionDate/missionDate.user.js
+++ modules/lss-missionDate/missionDate.user.js
@@ -1,5 +1,4 @@
 (function() {
-    
     I18n.translations.de['lssm']['missionDate'] = {
         ago: 'Vor',
         created: 'Einsatz eingegangen: ',
@@ -23,27 +22,46 @@
     {
         var einsatzdate = $('#missionH1').data('original-title');
         console.log("MD 1: "+einsatzdate);
-        if(I18n.locale == 'de')
+        if(I18n.locale === 'de')
         {
-            h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+' <span id="einsatzdate"></span></small><br>');
-            einsatzdate = einsatzdate.replace(/ Januar,/i, ' January 2017 ').replace(/ Februar,/i, ' February 2017 ').replace(/ März,/i, ' March 2017 ').replace(/ April,/i, 'April 2017 ').replace(/ Mai,/i, ' May 2017 ').replace(/ Juni,/i, ' June 2017 ').replace(/ Juli,/i, ' July 2017 ').replace(/ August,/i, 'August 2017 ').replace(/ September,/i, 'September 2017 ').replace(/ Oktober,/i, ' October 2017 ').replace(/ November,/i, 'November 2017 ').replace(/ Dezember,/i, ' December 2017 ');
+            h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+
+                                  '<span id="einsatzdate"></span></small><br>');
+            
+            einsatzdate = einsatzdate
+                            .replace(/ Januar,/i, ' January 2017 ').replace(/ Februar,/i, ' February 2017 ').replace(/ März,/i, ' March 2017 ')
+                            .replace(/ April,/i, 'April 2017 ').replace(/ Mai,/i, ' May 2017 ').replace(/ Juni,/i, ' June 2017 ')
+                            .replace(/ Juli,/i, ' July 2017 ').replace(/ August,/i, 'August 2017 ').replace(/ September,/i, 'September 2017 ')
+                            .replace(/ Oktober,/i, ' October 2017 ').replace(/ November,/i, 'November 2017 ').replace(/ Dezember,/i, ' December 2017 ');
         }
         else
         {
-            h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span> '+I18n.t('lssm.missionDate.ago')+'</small><br>');
-            einsatzdate = einsatzdate.replace(/ Jan /i, ' January 2017 ').replace(/ Feb /i, ' February 2017 ').replace(/ Mar /i, ' March 2017 ').replace(/ Apr /i, ' April 2017 ').replace(/ May /i, ' May 2017 ').replace(/ Jun /i, ' June 2017 ').replace(/ Jul /i, ' July 2017 ').replace(/ Aug /i, ' August 2017 ').replace(/ Sep /i, ' September 2017 ').replace(/ Okt /i, ' October 2017 ').replace(/ Nov /i, ' November 2017 ').replace(/ Dec /i, ' December 2017 ');
+            h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span> '+
+                                  I18n.t('lssm.missionDate.ago')+'</small><br>');
+            
+            einsatzdate = einsatzdate
+                            .replace(/ Jan /i, ' January 2017 ').replace(/ Feb /i, ' February 2017 ').replace(/ Mar /i, ' March 2017 ')
+                            .replace(/ Apr /i, ' April 2017 ').replace(/ May /i, ' May 2017 ').replace(/ Jun /i, ' June 2017 ')
+                            .replace(/ Jul /i, ' July 2017 ').replace(/ Aug /i, ' August 2017 ').replace(/ Sep /i, ' September 2017 ')
+                            .replace(/ Okt /i, ' October 2017 ').replace(/ Nov /i, ' November 2017 ').replace(/ Dec /i, ' December 2017 ');
         }                    
         
         var currDate = new Date();
-        var tempOlddate = einsatzdate.replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'').replace('.','')+":00";      
-        missionDate = new Date(tempOlddate);
+        var tempOlddate = einsatzdate
+                            .replace(I18n.t('lssm.missionDate.time_postfix'),'')
+                            .replace(I18n.t('lssm.missionDate.created'),'')
+                            .replace('.','')+":00";   
+        
+        var missionDate = new Date(tempOlddate);
         missionDate.setFullYear(currDate.getFullYear());
     
         var timeDiff = currDate.getTime() - missionDate.getTime();
         
         if(timeDiff < 0)
         {
-            tempOlddate = h1.getAttribute("data-original-title").replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
+            tempOlddate = h1.getAttribute("data-original-title")
+                            .replace(I18n.t('lssm.missionDate.time_postfix'),'')
+                            .replace(I18n.t('lssm.missionDate.created'),'');
+            
             missionDate = new Date(tempOlddate);
             missionDate.setFullYear(currDate.getFullYear()-1);
             
@@ -63,7 +81,7 @@
         else
             newDay = '';
         
-        if (I18n.locale == 'en')
+        if (I18n.locale === 'en')
         {
             var offset = currDate.getTimezoneOffset()/60;
             //Zahl 4, weil EST UTC+4 ist
