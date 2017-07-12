f21bf767b128653d258263a142083fd1c709b4ab
Jan <Jan.litzenburger@gmail.com>
2017-07-12 13:43:19 +0200

    183 mission date fix (#204)
    * #183 Replace short month names
    
    Replace short month names with the full name, so the date creation works.
    
    * Spacing Fix
    
    * Logging
    
    * Fix for mission Year in MC and MEL
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Fix for FF not recognizing german month names
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Sonar Fixes
    
    * Sonar Fixes 2
    
    * Sonar Fixes 3
    
    * Removed console.log
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * Update missionDate.user.js
    
    * 183: Review changes
    
    * 183: UI Fixes
    
    * 183: RegEx fixes
    
    * 183: Fixed Spaces
    
    * 183: Sonar findings
    
    * 183: Added String Format Helper Function; Prettify code
    
    * 183: Pretty Format with atom.io

--- modules/lss-missionDate/missionDate.user.js
+++ modules/lss-missionDate/missionDate.user.js
@@ -1,79 +1,90 @@
 (function() {
-	
-	I18n.translations.de['lssm']['missionDate'] = {
-        ago: 'Vor',
-		created: 'Einsatz eingegangen: ',
-		time_postfix: ' Uhr'
-    };
+  I18n.translations.de['lssm']['missionDate'] = {
+    ago: 'Vor',
+    months: ['Januar', 'Februar', 'März', 'April', 'Mai', 'Juni', 'Juli', 'August', 'September', 'Oktober', 'November', 'Dezember'],
+    dateRegex: /([0-9]{2})\. (.*), ([0-9]{2}):([0-9]{2})/i
+  };
 
-    I18n.translations.en['lssm']['missionDate'] = {
-        ago: 'ago',
-		created: 'Created at: ',
-		time_postfix: ' Uhr'
-    };
-	
-	I18n.translations.nl['lssm']['missionDate'] = {
-        ago: 'geleden',
-		created: 'Inzet begonnen: ',
-		time_postfix: ' Uhr'
-    };
-	
-    var h1 = document.getElementById('missionH1');
-	if (h1 !== null)
-    {
-		var einsatzdate = $('#missionH1').data('original-title');
-		if(I18n.locale == 'de')
-		{
-			h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - '+I18n.t('lssm.missionDate.ago')+' <span id="einsatzdate"></span></small><br>');
-		}
-		else
-		{
-			h1.insertAdjacentHTML('afterend', '<small>'+einsatzdate+' - <span id="einsatzdate"></span> '+I18n.t('lssm.missionDate.ago')+'</small><br>');
-		}
-		
-		
-		var currDate = new Date();
-		var tempOlddate = einsatzdate.replace(',', ' '+currDate.getFullYear()+',').replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
-		missionDate = new Date(tempOlddate);
-	
-		var timeDiff = currDate.getTime() - missionDate.getTime();
-		
-		if(timeDiff < 0)
-		{
-			tempOlddate = h1.getAttribute("data-original-title").replace(',', ' '+currDate.getFullYear()-1+',').replace(I18n.t('lssm.missionDate.time_postfix'),'').replace(I18n.t('lssm.missionDate.created'),'');
-			missionDate = new Date(tempOlddate);
-			timeDiff = currDate.getTime() - xDate.getTime();
-		}
-		
-		var minutes = (timeDiff/1000)/60;
-		var hours = minutes/60;
-		var days = hours/24;
-						
-		var newDay = Math.floor(days);
-		var newHour = Math.floor(hours % 24);
-		var newMin = Math.floor(minutes % 60);
-		
-		if (newDay >= 1)
-			newDay = newDay + 'd ';
-		else
-			newDay = '';
-		
-	    if (I18n.locale == 'en')
-		{
-		    var offset = currDate.getTimezoneOffset()/60;
-		    //Zahl 4, weil EST UTC+4 ist
-		    var newOffset = 4 - offset;
-	    	    newHour -= newOffset;
-    	}
-	    
-		if (newHour >= 1)
-			newHour = newHour + 'h ';
-		else
-			newHour = '';
-	    
-		if (newMin < 0)
-			newMin = 0;
-		if (document.getElementById('einsatzdate') !== null)
-			document.getElementById('einsatzdate').innerHTML = newDay + newHour + newMin + ' min';
-	}
+  I18n.translations.en['lssm']['missionDate'] = {
+    ago: 'ago',
+    months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    dateRegex: /([0-9]{2}) (.*) ([0-9]{2}):([0-9]{2})/i
+  };
+
+  I18n.translations.nl['lssm']['missionDate'] = {
+    ago: 'geleden',
+    months: ['jan', 'feb', 'maart', 'apr', 'mei', 'jun', 'jul', 'aug', 'sep', 'okt', 'nov', 'dec'],
+    dateRegex: /([0-9]{2}) (.*) ([0-9]{2}):([0-9]{2})/i
+  };
+
+  function parseMissionDate(dateString) {
+    var matches = dateString.match(I18n.t('lssm.missionDate.dateRegex'));
+    var day = matches[1];
+    var month = matches[2];
+    var hour = matches[3];
+    var minute = matches[4];
+
+    // Transform month name to numeric month
+    var months = I18n.t("lssm.missionDate.months")
+    month = months.indexOf(month);
+
+    var today = new Date();
+
+    var year = today.getFullYear();
+
+    var date = new Date(year, month, day, hour, minute, 0, 0);
+
+    // Make plausibility check. If diff is negative is has to be at least the year before.
+    if (today.getTime() - date.getTime() < 0) {
+      date.setFullYear(date.getFullYear() - 1);
+    }
+    return date;
+  }
+
+  // Just execute the script when there is a mission Headline
+  var missionDate = $('#missionH1').length > 0 ? $('#missionH1').data('original-title') : null;
+  if (missionDate != null) {
+    // Parse mission date to Date() object
+    var parsedMissionDate = parseMissionDate(missionDate);
+
+    var today = new Date();
+    var timeDiff = today.getTime() - parsedMissionDate.getTime();
+
+    var minutes = (timeDiff / 1000) / 60;
+    var hours = minutes / 60;
+    var days = hours / 24;
+
+    var newDay = Math.floor(days);
+    var newHour = Math.floor(hours % 24);
+    var newMin = Math.floor(minutes % 60);
+
+    var timeGone = "";
+    if (newDay > 0)
+      timeGone += String.format(' {0} d', newDay);
+
+    if (I18n.locale === 'en') {
+      var offset = today.getTimezoneOffset() / 60;
+      // Zahl 4, weil EST UTC+4 ist
+      var newOffset = 4 - offset;
+      newHour -= newOffset;
+    }
+
+    if (newHour > 0) {
+      timeGone += String.format(' {0} h', newHour);
+    }
+
+    if (newMin > 0) {
+      timeGone += String.format(' {0} min', newMin);
+    }
+
+    var markup;
+    if (I18n.locale === 'de') {
+      markup = '{0} - <span>{1} {2}</span>';
+    } else {
+      markup = '{0} - <span>{2} {1}</span>';
+    }
+
+    markup = String.format(markup, missionDate, I18n.t("lssm.missionDate.ago"), timeGone)
+    $('#missionH1').after('<div><small>' + markup + '</small></div>');
+  }
 })();
