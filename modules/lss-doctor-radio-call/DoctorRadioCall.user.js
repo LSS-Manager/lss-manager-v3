ebe494e97c7f4c52e69334d369301549000c6a20
Jalibu <dev@localhost>
2017-07-12 12:49:54 +0200

    Atom.io prettyformat

--- modules/lss-doctor-radio-call/DoctorRadioCall.user.js
+++ modules/lss-doctor-radio-call/DoctorRadioCall.user.js
@@ -1,42 +1,42 @@
 (function(I18n, $) {
-	'use strict';
+  'use strict';
 
-	I18n.translations.de.lssm.doctorradiocall = {
-		nef : "NEF"
-	};
-	I18n.translations.en.lssm.doctorradiocall = {
-		nef : "HEMS"
-	};
-	I18n.translations.nl.lssm.doctorradiocall = {
-		nef : "MMT-Auto"
-	};
+  I18n.translations.de.lssm.doctorradiocall = {
+    nef: "NEF"
+  };
+  I18n.translations.en.lssm.doctorradiocall = {
+    nef: "HEMS"
+  };
+  I18n.translations.nl.lssm.doctorradiocall = {
+    nef: "MMT-Auto"
+  };
 
-	// Hook in Patient Marker function to retrieve event
-	var patientMarkerAddOrig = patientMarkerAdd;
+  // Hook in Patient Marker function to retrieve event
+  var patientMarkerAddOrig = patientMarkerAdd;
 
-	patientMarkerAdd = function(t) { // NOSONAR: Variable is declared globally in the main game.
-		if (t.missing_text && t.missing_text.indexOf(I18n.t('lssm.doctorradiocall.nef')) >= 0) {
-			radioMessage({
-				"mission_id" : t.mission_id,
-				"additionalText" : "",
-				"user_id" : user_id,
-				"type" : "vehicle_fms",
-				"id" : "",
-				"fms_real" : 5,
-				"fms" : 5,
-				"fms_text" : t.missing_text,
-				"caption" : ""
-			});
+  patientMarkerAdd = function(t) { // NOSONAR: Variable is declared globally in the main game.
+    if (t.missing_text && t.missing_text.indexOf(I18n.t('lssm.doctorradiocall.nef')) >= 0) {
+      radioMessage({
+        "mission_id": t.mission_id,
+        "additionalText": "",
+        "user_id": user_id,
+        "type": "vehicle_fms",
+        "id": "",
+        "fms_real": 5,
+        "fms": 5,
+        "fms_text": t.missing_text,
+        "caption": ""
+      });
 
-			// Due we do not get a calling vehicle_id we have to remove the
-			// corresponding button manually.
-			var el = $('#radio_messages_important').find('a[href="/vehicles/"]');
-			el.parent().find('a').click(function() {
-				$(this).parent().hide();
-			});
-			el.remove();
-		}
-		patientMarkerAddOrig(t);
-	};
+      // Due we do not get a calling vehicle_id we have to remove the
+      // corresponding button manually.
+      var el = $('#radio_messages_important').find('a[href="/vehicles/"]');
+      el.parent().find('a').click(function() {
+        $(this).parent().hide();
+      });
+      el.remove();
+    }
+    patientMarkerAddOrig(t);
+  };
 
 })(I18n, jQuery);
