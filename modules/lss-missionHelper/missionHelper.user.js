860f10a622b9b69dc89ac606e1ca93084926a8ad
Tobias <Suchty112@users.noreply.github.com>
2017-07-14 10:46:31 +0200

    Update for Missions (MC,Mel and LSS)
    Update for Missions (MC,Mel and LSS)

--- modules/lss-missionHelper/missionHelper.user.js
+++ modules/lss-missionHelper/missionHelper.user.js
@@ -504,7 +504,10 @@
 				'evtl. auch 1x RTH (50%)</br>1x Löschfahrzeug (20%)</br>1x GW-Taucher (10%)' +
 				'</br></br><span class="badge">Credits: ~ 1500 </span>',
 				299 : '5x Löschfahrzeuge (100%)</br>1x Einsatzleitwagen 1 (100%)</br>1x Drehleiter (75%)</br>' +
-				'1x GW-Atemschutz (100%)</br>1x Schlauchwagen (50%)</br></br><span class="badge">Credits: ~ 2500 </span>'
+				'1x GW-Atemschutz (100%)</br>1x Schlauchwagen (50%)</br></br><span class="badge">Credits: ~ 2500 </span>',
+				300 : '2x Löschfahrzeuge (100%)</br>1x GW-Höhenrettung (50%)</br>1x Funkstreifenwagen (100%)' +
+				'</br>1x Einsatzleitwagen 1</br>1x Drehleiter (75%)</br>1x Rettungstransportwagen (100%)' +
+				'1x Notarzteinsatzfahrzeug (90%)/1x Rettungshubschrauber (50%)'
 			}
 		}
 
@@ -684,7 +687,8 @@
 				'</br></br><span class="badge">Credits: ~ 7000 </span>',
 				128 : '1x Police Car (100%)</br></br><span class="badge">Credits: ~ 600 </span>',
 				129 : '1x Police Car (100%)</br></br><span class="badge">Credits: ~ 600 </span>',
-				130 : '1x Firetruck (100%)</br></br><span class="badge">Credits: ~ 400 </span>'
+				130 : '1x Firetruck (100%)</br></br><span class="badge">Credits: ~ 400 </span>',
+				131 : '1x Police Car (100%)'
 			}
 		}
 
@@ -1048,6 +1052,7 @@
 				'</br>1x Slangenwagen (50%)</br>1x Adembeschermingsvoertuig (20%)</br>1x Adviseurs Gevaarlijke Stoffen (10%)' +
 				'</br>1x Waarschuwings- en Verkenningsdienst(10%)</br>1x Politieauto (100%)</br>' +
 				'</br><span class="badge">Credits: ~ 1700 </span>',
+				230 : '1x Ambulance (100%)'
 			}
 		}
 	}
