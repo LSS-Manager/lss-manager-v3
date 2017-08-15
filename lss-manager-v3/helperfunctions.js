e97a4454cb96e73284f55e7ad7bd56ba5c78802a
Tobias <Suchty112@users.noreply.github.com>
2017-08-15 23:35:05 +0200

    console readded aber auskommentiert

--- lss-manager-v3/helperfunctions.js
+++ lss-manager-v3/helperfunctions.js
@@ -257,6 +257,7 @@ lssm.newDragableDivOnMap=function(id, classe, pos) {
 
 // Add custom String format function
 if (!String.format) {
+//	console.log("execute");
   String.format = function(format) {
     var args = Array.prototype.slice.call(arguments, 1);
     return format.replace(/{(\d+)}/g, function(match, number) {
