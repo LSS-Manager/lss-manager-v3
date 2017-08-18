178ea88af4c21ca873101fe7beb4e6018c720b72
Tobias <Suchty112@users.noreply.github.com>
2017-08-18 15:32:48 +0200

    Added the new missions at 18.8

--- modules/lss-missionHelper/missionHelper.user.js
+++ modules/lss-missionHelper/missionHelper.user.js
@@ -255,7 +255,7 @@
 				'1x Drehleiter (50%)</br>1x Funkstreifenwagen (100%)</br></br><span class="badge">Credits: ~ 1400 </span>',
 				168 : '1x Löschfahrzeug (100%)</br>1x Funkstreifenwagen (100%)</br></br><span class="badge">Credits: ~ 600</span>',
 				169 : '1x Löschfahrzeug (100%)</br>1x Funkstreifenwagen (100%)</br></br><span class="badge">Credits: ~ 600</span>',
-				170 : '1x Löschfahrzeug (100%)</br>2x Funkstreifenwagen (100%)</br></br><span class="badge">Credits: ~ 600</span>',
+				170 : '1x Löschfahrzeug (100%)</br>1x Funkstreifenwagen (100%)</br></br><span class="badge">Credits: ~ 600</span>',
 				171 : '1x Löschfahrzeug (100%)</br>1x Schlauchwagen (100%)</br>1x Einsatzleitwagen 2 (100%)</br>' +
 				'1x GW-Öl (100%)</br>2x Funkstreifenwagen (100%)</br></br><span class="badge">Credits: ~ 4000 </span>',
 				172 : '1x Löschfahrzeug (100%)</br></br><span class="badge">Credits: ~ 600 </span>',
@@ -522,8 +522,9 @@
 				'</br>3x WaWe 10 (100%)',
 				306 : '<h4>ABSICHERUNG</h4>8x Funkstreifenwagen (100%)</br>2x WaWe 10 (100%)</br>4x leBefKw (100%)</br>' +
 				'9x GruKw (100%)</br>1x FüKw (100%)</br>2x Löschfahrzeuge (100%)</br>4x Rettungstransportwagen (100%)',
-				307 : '7x Funkstreifenwagen (100%)</br>3x WaWe 10 (100%)</br>9x GruKw (100%)</br>4x leBefKw (100%)' + 
-				'</br>1x FüKw (100%)</br>1x Polizeihubschrauber (100%)<span class="badge">Credits: ~ 6000 </span>'
+				307 : '7x Funkstreifenwagen (100%)</br>3x WaWe 10 (100%)</br>9x GruKw (100%)</br>4x leBefKw (100%)' +
+				'</br>1x FüKw (100%)</br>1x Polizeihubschrauber (100%)</br></br><span class="badge">Credits: ~ 6000 </span>',
+				308 : '2x Löschfahrzeuge (100%)</br></br><span class="badge">Credits: ~ 1000 </span>'
 			}
 		}
 
@@ -710,9 +711,10 @@
 				133 : '1x Police Car (100%)</br></br><span class="badge">Credits: ~ 400 </span>',
 				134 : '2x Police Cars (100%)</br>1x K9-Unit (100%)</br></br><span class="badge">Credits: ~ 1000</span>',
 				135 : '4x Police Cars (100%)</br>1x K9-Unit (100%)</br></br><span class="badge">Credits: ~ 2200</span>',
-				136 : '7x Police Cars (100%)</br>1x K9-Unit (100%)</br>1x Police Helicopter (100%)' + 
+				136 : '7x Police Cars (100%)</br>1x K9-Unit (100%)</br>1x Police Helicopter (100%)' +
 				'</br></br><span class="badge">Credits: ~ 3000</span>',
-				137 : '4x Police Cars (100%)</br>1x K9-Unit (100%)</br></br><span class="badge">Credits: ~ 1500</span>'
+				137 : '4x Police Cars (100%)</br>1x K9-Unit (100%)</br></br><span class="badge">Credits: ~ 1500</span>',
+				138 : '1x Firetruck (100%)</br>1x Platform Truck (100%)</br>1x Battalion Chief (100%)</br></br><span class="badge">Credits: ~ 700</span>'
 			}
 		}
 
@@ -1084,7 +1086,8 @@
 				234 : '11x Tankautospuiten (100%)</br>1x Redvoertuig (100%)</br>5x Hulpverleningsvoertuigen (100%)</br>' +
 				'1x Slangenwagen (100%)</br>1x Adviseurs Gevaarlijke Stoffen (100%)</br>3x OvD-Bs (100%)</br>' +
 				'2x HOVDs (100%)</br>2x Commandowagen (100%)</br>14x Politieautos (100%)</br>' +
-				'min. 55 Ambulances</br>min. 1x MMT-Auto (25%)<span class="badge">Credits: ~ 22000 </span>'
+				'min. 55 Ambulances</br>min. 1x MMT-Auto (25%)</br></br><span class="badge">Credits: ~ 22000 </span>',
+				235 : '1x Politieauto (100%)</br>1x Ambulance (100%)</br></br><span class="badge">Credits: ~ 400 </span>'
 			}
 		}
 	}
