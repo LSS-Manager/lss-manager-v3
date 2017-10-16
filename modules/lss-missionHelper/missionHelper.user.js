a93f4ce7ecffa12c5833c39b6fb348e22bb0a98c
Tobias <Suchty112@users.noreply.github.com>
2017-10-16 20:06:31 +0200

    fix one mel mission

--- modules/lss-missionHelper/missionHelper.user.js
+++ modules/lss-missionHelper/missionHelper.user.js
@@ -1140,7 +1140,7 @@
 				247 : '5x Politieautos (100%)</br>1x OvD-P (100%)</br>3x ME Groepsvoertuig (100%)</br>1x ME Commandovoertuig (100%)</br></br>' +
 				'<span class="badge">Credits: ~ 1200 </span>',
 				248 : '4x Politieautos (100%)</br>1x OvD-P (100%)</br>3x ME Groepsvoertuig (100%)</br>1x ME Commandovoertuig (100%)</br>1x Politie Helikopter (100 %)' +
-				'<span class="badge">Credits: ~ 1800 </span>'
+				'</br></br><span class="badge">Credits: ~ 1800 </span>'
 			}
 		}
 	}
