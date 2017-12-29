c7be693a1cc46136541d6cc38c65398488123bc1
Tobias <Suchty112@users.noreply.github.com>
2017-12-29 14:33:15 +0100

    Added LSS and MC Mission 29.12

--- modules/lss-missionKeyword/missionKeyword.user.js
+++ modules/lss-missionKeyword/missionKeyword.user.js
@@ -345,7 +345,8 @@
         322: 'Geplante Autobahnsperrung',
         323: 'Dehnfugenbrand',
         324: 'Brand in Baumarkt',
-        325: 'Absicherung Castor-Transport'
+        325: 'Absicherung Castor-Transport',
+        326: 'Handverletzung durch Feuerwerkskörper'
       }
     };
     I18n.translations.en.lssm.missionKeywordDefaults = {
@@ -518,7 +519,8 @@
         151: 'Freight Train Derailment',
         152: 'Freight Train Derailment',
         153: 'Barn Fire',
-        154: 'Chlorine leak'
+        154: 'Chlorine leak',
+        155: 'Fire in Hospital'
       }
     };
     I18n.translations.nl.lssm.missionKeywordDefaults = {
@@ -1145,7 +1147,8 @@
       322: 'POL 2',
       323: 'B 2',
       324: 'B 5',
-      325: 'SICHERHEITSWACHE'
+      325: 'SICHERHEITSWACHE',
+      326: 'RD 2'
     };
     var DEFAULT_AAO_EN = {
       0: 'FIRE 1',
@@ -1302,7 +1305,8 @@
       151: 'TECH TRAIN',
       152: 'TECH TRAIN',
       153: 'FIRE 3',
-      154: 'ABC 2'
+      154: 'ABC 2',
+      155: 'FIRE 4'
     };
     var DEFAULT_AAO_NL = {
       0: 'KLEINE BRAND',
