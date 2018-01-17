1345601e773dc8f1e8f974d26ed621e4e28e00b8
Jan Litzenburger <Jan.Litzenburger@de.ibm.com>
2018-01-17 07:06:10 -0800

    Show warning when there are unsupported vehicle combinations in aao

--- modules/lss-aao-time/lss-aao-time.user.js
+++ modules/lss-aao-time/lss-aao-time.user.js
@@ -1,4 +1,6 @@
 (($, win, I18n) => {
+    if (!location.pathname.match(/\/missions\/\d+/)) return;
+
     I18n.translations.de.lssm.aaotime = {
         timeRegex: /([0-9]{2}) Min. ([0-9]{2}) Sek./i,
         hourIdentifier: 'Std.',
@@ -8,6 +10,7 @@
         missingVehicles1: 'Für die folgenden Fahrzeuge konnte keine Anfahrtsdauer ermittelt werden:',
         missingVehicles2: 'Möglicherweise sind sie nicht verfügbar oder nicht mit Zeiten aufgelistet.',
         missingVehicles3: 'Fehler beim Berechnen der Anfahrtszeiten. Möglicherweise sind Fahrzeuge nicht verfügbar oder nicht mit Zeiten aufgelistet.',
+        unsupportedKeys: 'Die Dauer konnte nicht berechnet werden, da diese AAO Fahrzeug-Kombinationen enthält, die von diesem Modul leider noch nicht unterstützt werden: {0}',
         timeFormat: '{0} min {1} sek',
         timeFormatWithHours: '{0} h {1} min {2} sek',
     };
@@ -20,6 +23,7 @@
         missingVehicles1: 'For the following vehicles it was not possible to determine the time:',
         missingVehicles2: 'They might not be present or listed with a time.',
         missingVehicles3: 'Error calculating the time. The required vehicles might not be present or listed with a time.',
+        unsupportedKeys: 'The duration could not be determined because this ARR contains vehicle combinations that are not yet supported by this module: {0}',
         timeFormat: '{0} min {1} sec',
         timeFormatWithHours: '{0} h {1} min {2} sec',
     };
@@ -32,14 +36,14 @@
         missingVehicles1: 'Geen nadere tijd kan worden bepaald voor de volgende voertuigen:',
         missingVehicles2: 'Zij zijn mogelijk niet beschikbaar of tijdelijk vermeld.',
         missingVehicles3: 'Fout bij de berekening van de aanlooptijden. Voertuigen zijn mogelijk niet beschikbaar of mogen tijdelijk worden vermeld.',
+        unsupportedKeys: 'This AUR contains combinations that are not yet supported by this module: {0}',
         timeFormat: '{0} min {1} s',
         timeFormatWithHours: '{0} h {1} min {2} s',
     };
 
     // Prepare aaoTitle
-    const aaoTitle = $('<div id="aaoTitle" style="display:none; max-width: 260px; padding: 8px; background-color:#5CB85C">');
+    const aaoTitle = $('<div id="aaoTitle" style="display:none; z-index: 999; max-width: 260px; padding: 8px; background-color:#5CB85C">');
     $('body').append(aaoTitle);
-
     const formatSeconds = (totalSeconds) => {
         const days = Math.floor(totalSeconds / 86400);
         totalSeconds -= days * 86400;
@@ -60,21 +64,39 @@
 
     const prettyPrintResult = (aaoObject, aaoVehicles, secondsString) => {
         let response;
+        let unsupportedKeys = [];
+        for (let key in aaoVehicles) {
+            if (aaoVehicles[key] === -1) {
+                unsupportedKeys.push(key);
+            }
+        }
         if (Object.keys(aaoVehicles).length === 0) {
             response = String.format(I18n.t("lssm.aaotime.duration"), formatSeconds(secondsString));
         } else if (secondsString > 0) {
             response = String.format(I18n.t("lssm.aaotime.leastTime"), formatSeconds(secondsString)) + '<br/>';
-            response += I18n.t("lssm.aaotime.missingVehicles1") + '<br/><ul>';
-            for (let key in aaoVehicles) {
-                response += '<li>' + aaoVehicles[key] + 'x ' + key + '</li>';
+            if (unsupportedKeys.length > 0) {
+                response = String.format(I18n.t("lssm.aaotime.unsupportedKeys"), unsupportedKeys.toString());
+            } else {
+                response += I18n.t("lssm.aaotime.missingVehicles1") + '<br/><ul>';
+                for (let key in aaoVehicles) {
+                    response += '<li>' + aaoVehicles[key] + 'x ' + key + '</li>';
+                }
+                response += '</ul>' + I18n.t("lssm.aaotime.missingVehicles2");
             }
-            response += '</ul>' + I18n.t("lssm.aaotime.missingVehicles2");
         } else {
-            response = I18n.t("lssm.aaotime.missingVehicles3");
+            if (unsupportedKeys.length > 0) {
+                response = String.format(I18n.t("lssm.aaotime.unsupportedKeys"), unsupportedKeys.toString());
+            } else {
+                response = I18n.t("lssm.aaotime.missingVehicles3");
+            }
         }
         aaoTitle.html(response);
         const aaoPosition = $(aaoObject).offset();
-        aaoTitle.css({top: aaoPosition.top + 30, left: aaoPosition.left, position: 'absolute'}).show();
+        aaoTitle.css({
+            top: aaoPosition.top + 30,
+            left: aaoPosition.left,
+            position: 'absolute'
+        }).show();
     };
 
     const getDurationInSeconds = (vehicleObject) => {
@@ -123,23 +145,27 @@
         $('.vehicle_checkbox').each(function () {
             const vehicle = this;
             for (let key in aaoVehicleSearchList) {
-                if (vehicle.attributes[key].value > 0) { // Does that vehicle meet an AAO requirement?
-                    const totalSeconds = getDurationInSeconds(vehicle);
-                    if (totalSeconds === null) {
-                        break; // Skip that vehicle if its time could not be determined
-                    }
-                    aaoVehicleSearchList[key]--;
+                if (key in vehicle.attributes) {
+                    if (vehicle.attributes[key].value > 0) { // Does that vehicle meet an AAO requirement?
+                        const totalSeconds = getDurationInSeconds(vehicle);
+                        if (totalSeconds === null) {
+                            break; // Skip that vehicle if its time could not be determined
+                        }
+                        aaoVehicleSearchList[key]--;
 
-                    if (totalSeconds > maximumSeconds) {
-                        maximumSeconds = totalSeconds;
-                    }
+                        if (totalSeconds > maximumSeconds) {
+                            maximumSeconds = totalSeconds;
+                        }
 
-                    if (aaoVehicleSearchList[key] === 0) {
-                        delete aaoVehicleSearchList[key]; // Remove the vehicle from the search list.
-                        if (Object.keys(aaoVehicleSearchList).length === 0) { // If there aren't any missing vehicles, we're done! Hurray!
-                            return false;
+                        if (aaoVehicleSearchList[key] === 0) {
+                            delete aaoVehicleSearchList[key]; // Remove the vehicle from the search list.
+                            if (Object.keys(aaoVehicleSearchList).length === 0) { // If there aren't any missing vehicles, we're done! Hurray!
+                                return false;
+                            }
                         }
                     }
+                } else {
+                    aaoVehicleSearchList[key] = -1;
                 }
             }
         });
