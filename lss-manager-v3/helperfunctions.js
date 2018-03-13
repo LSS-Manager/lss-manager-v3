5c88176215f91848c891122574251d271b6e98ba
Tobias <Suchty112@users.noreply.github.com>
2018-03-13 19:32:21 +0100

    New Cars to HF

--- lss-manager-v3/helperfunctions.js
+++ lss-manager-v3/helperfunctions.js
@@ -76,7 +76,8 @@ else if (I18n.locale == "nl")
         "41": ["CT (8x8) | Crashtender (8x8)", 0],
         "42": ["CT (6x6) | Crashtender (6x6)", 0],
         "43": ["CT (4x4) | Crashtender (4x4)", 0],
-        "44": ["AFO/OSC | Airport Fire Officer / On Scene Commander", 0]
+        "44": ["AFO/OSC | Airport Fire Officer / On Scene Commander", 0],
+        "45": ["DBH | Dompelpomphaakarmbak", 0]
     }
 else if (I18n.locale == "de")
     lssm.carsById = {
@@ -157,7 +158,8 @@ else if (I18n.locale == "de")
         "74": ["NAW", 1],
         "75": ["FLF", 0],
         "76": ["Rettungstreppe", 0],
-        "77": ["AB-Gefahrgut", 0]
+        "77": ["AB-Gefahrgut", 0],
+        "78": ["AB-Einsatzleitung", 0]
     };
 
 lssm.getVehicleNameById = function(vehicleId) {
