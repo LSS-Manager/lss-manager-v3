1f77d0019aa19cb9efc3a7986712a8c9557ea49a
Jan <jan.boehmerle@ledbrain.de>
2018-05-10 22:37:19 +0200

    Neue Funktion für Module
    "isActive" gibt aus, ob ein Modul aktiv ist oder nicht.

--- lss-manager-v3/lss-manager-v3.dev.js
+++ lss-manager-v3/lss-manager-v3.dev.js
@@ -1282,6 +1282,9 @@ lssm.modules = {
         } catch (e) {
             console.log("On lssm_module load: " + e.message);
         }
+    },
+    isActive: function(e) {
+        return lssm.Module[e].active;
     }
 };
 
