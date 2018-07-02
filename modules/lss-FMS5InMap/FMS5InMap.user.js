2302eb9a0b3134ff4feeceacaaf2ba6c6e8e1a70
Jan <jan.boehmerle@ledbrain.de>
2018-07-02 11:14:41 +0200

    Mapkit-Notification

--- modules/lss-FMS5InMap/FMS5InMap.user.js
+++ modules/lss-FMS5InMap/FMS5InMap.user.js
@@ -1,21 +1,29 @@
-(function ($) {
+(function ($, I18n) {
+  I18n.translations.de.lssm.fms5inmap = {
+		mapkit : "[FMS5InMap]\n\n Mapkit wird aktuell noch nicht von uns unterstützt.\n\nBitte deaktiviere das Addon solange oder schalte wieder \nauf OpenStreeMap um",
+	}
+  I18n.translations.en.lssm.fms5inmap = {
+    mapkit : "[FMS5InMap]\n\n Mapkit is not currently supported by us.\n\nPlease turn this Add-On off or change back to OpenStreetMap",
+  }
+  I18n.translations.nl.lssm.fms5inmap = {
+    mapkit : "[FMS5InMap]\n\n Mapkit wordt op dit moment niet door ons ondersteund.\n\nSchakel deze addon uit of keer terug naar OpenStreetMap",
+  }
     var radio_message_important = $("#radio_messages_important");
     var id = "fm5InMap";
     if ("undefined" != typeof mapkit) {
-      alert("[" + I18n.t('lssm.Module.FMS5InMap.name') + "]\n\n" + I18n.t('lssm.mapkit'));
+        alert("[" + lssm.Module.fms5inmap.name[I18n.locale] + "]\n\n" + I18n.t('lssm.mapkit'));
     } else {
-        lssm.newDragableDivOnMap(id,"",lssm.settings.get(lssm.config.prefix + "_"+id+"Position",{x:0,y:0})).append(radio_message_important);
-        radio_message_important.css({
-            "z-index": "999",
-            "opacity": "0.9",
-            "bottom": "0px",
-            "left": "0px",
-            "display": "block",
-            "list-style-type": "none",
-            "width": "100%",
-            "color": "rgba(0, 0, 0, 0)",
-            "margin": "0 !important",
-            "padding": "10px"
-        });
-    }
-})($);
+    lssm.newDragableDivOnMap(id,"",lssm.settings.get(lssm.config.prefix + "_"+id+"Position",{x:0,y:0})).append(radio_message_important);
+    radio_message_important.css({
+        "z-index": "999",
+        "opacity": "0.9",
+        "bottom": "0px",
+        "left": "0px",
+        "display": "block",
+        "list-style-type": "none",
+        "width": "100%",
+        "color": "rgba(0, 0, 0, 0)",
+        "margin": "0 !important",
+        "padding": "10px"
+    });
+}})($, I18n);
