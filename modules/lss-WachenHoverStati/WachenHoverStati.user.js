1057fc951b0370cc13c301fc6ae51954d69685e3
Jan <jan.boehmerle@ledbrain.de>
2018-07-02 10:43:35 +0200

    Mapkit-Notification-Translation-Fix

--- modules/lss-WachenHoverStati/WachenHoverStati.user.js
+++ modules/lss-WachenHoverStati/WachenHoverStati.user.js
@@ -2,37 +2,41 @@
  * Created by Northdegree on 05.06.2017.
  */
 (function ($, I18n) {
-    $(document).bind(lssm.hook.postname("building_maps_draw"),function(event,e){
-        "use strict";
-        redraw_Labels();
-    });
-    $(document).bind(lssm.hook.postname("radioMessage"),function(event,t){
-        "use strict";
-        redraw_Labels();
-    });
-    function redraw_Labels() {
-        $.each(building_markers, function (key, marker) {
-            // Is the tooltip currently open?
-            var reopen = false;
-            if (marker.isTooltipOpen()) {
-                reopen = true;
-                // Close the tooltip
-                marker.closeTooltip();
-            }
-            // Unbind the tooltip and edit the content
-            marker.unbindTooltip();
-            c_labels = lssm.car_list(marker.building_id);
-            data = marker.options.title;
-            if (c_labels.length > 0) {
-                data += '&nbsp;<i class="fa fa-car"></i>' + c_labels.length;
-                data += lssm.car_list_printable(c_labels);
-            }
-            // Re-Bind the content
-            marker.bindTooltip(data, {direction: "top", offset: L.point(0,-marker.options.icon.options.iconSize[1]),zIndex: 999});
-            // Open the new tooltip (if needed)
-            if (reopen)
-                marker.openTooltip();
+    if ("undefined" != typeof mapkit) {
+        alert("[" + I18n.t('lssm.Module.WachenHoverStati.name') + "]" + I18n.t('lssm.mapkit'));
+    } else {
+        $(document).bind(lssm.hook.postname("building_maps_draw"),function(event,e){
+            "use strict";
+            redraw_Labels();
         });
+        $(document).bind(lssm.hook.postname("radioMessage"),function(event,t){
+            "use strict";
+            redraw_Labels();
+        });
+        function redraw_Labels() {
+            $.each(building_markers, function (key, marker) {
+                // Is the tooltip currently open?
+                var reopen = false;
+                if (marker.isTooltipOpen()) {
+                    reopen = true;
+                    // Close the tooltip
+                    marker.closeTooltip();
+                }
+                // Unbind the tooltip and edit the content
+                marker.unbindTooltip();
+                c_labels = lssm.car_list(marker.building_id);
+                data = marker.options.title;
+                if (c_labels.length > 0) {
+                    data += '&nbsp;<i class="fa fa-car"></i>' + c_labels.length;
+                    data += lssm.car_list_printable(c_labels);
+                }
+                // Re-Bind the content
+                marker.bindTooltip(data, {direction: "top", offset: L.point(0,-marker.options.icon.options.iconSize[1]),zIndex: 999});
+                // Open the new tooltip (if needed)
+                if (reopen)
+                    marker.openTooltip();
+            });
+        }
+        redraw_Labels();
     }
-    redraw_Labels();
 })($, I18n);
