627b984ccc3652c301c7de336ef1a0184aefc438
Tobias <Suchty112@users.noreply.github.com>
2019-03-11 22:29:56 +0100

    fdsf

--- modules/lss-aao-time/lss-aao-time.user.js
+++ modules/lss-aao-time/lss-aao-time.user.js
@@ -126,7 +126,7 @@
     const getAaoVehicles = (aaoObject) => {
         let vehicles = {};
         $.each(aaoObject.attributes, function (index, attribute) {
-            if (typeof(attribute) != "undefined" && typeof(attribute.value) != "undefined" && typeof(attribute.name) != "undefined") {
+            if (typeof(attribute) != "undefined" && typeof(attribute.value) != "undefined" && typeof(attribute.name) != "building_ids") {
                 vehicles[attribute.name] = parseInt(attribute.value);
             }
         });
