c68ca9d85fbf1f33a98eec28d9de80dfb650c4e4
Tobias <Suchty112@users.noreply.github.com>
2019-03-26 17:27:00 +0100

    Blödes github...

--- modules/lss-aao-time/lss-aao-time.user.js
+++ modules/lss-aao-time/lss-aao-time.user.js
@@ -126,14 +126,13 @@
     const getAaoVehicles = (aaoObject) => {
         let vehicles = {};
         $.each(aaoObject.attributes, function (index, attribute) {
-            if (typeof(attribute) != "undefined" && typeof(attribute.value) != "undefined" && typeof(attribute.name) != "building_ids") {
+            if (!isNaN(attribute.value) && attribute.value > 0 && attribute.name !== 'building_ids') {
                 vehicles[attribute.name] = parseInt(attribute.value);
             }
         });
 
         return vehicles;
     };
-//(!isNaN(attribute.value) && attribute.value > 0 && attribute.name !== 'building_ids')
     const handleMouseOver = (aaoObject) => {
         if ($(aaoObject).find('span').hasClass('label-danger')) {
             return false;
