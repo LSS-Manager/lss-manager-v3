70021af0088a75df346eb759f9407606c844ca86
Jan <jan.boehmerle@ledbrain.de>
2019-06-12 16:06:33 +0200

    Pfade...

--- modules/lss-missionHelper/writeMission.php
+++ modules/lss-missionHelper/writeMission.php
@@ -1,11 +1,11 @@
 <?php
-$missions = json_decode(file_get_contents("/var/www/vhosts/ledbrain.de/lssm.ledbrain.de/lss-manager-v3/dev/modules/lss-missionHelper/missions.".$_POST["lang"].".json"), true);
+$missions = json_decode(file_get_contents("./missions.".$_POST["lang"].".json"), true);
 
 $missions[$_POST["id"]] = $_POST["mission"];
 
 ksort($missions);
 
-$Handle = fopen("/var/www/vhosts/ledbrain.de/lssm.ledbrain.de/lss-manager-v3/dev/modules/lss-missionHelper/missions.".$_POST["lang"].".json", 'w');
+$Handle = fopen("./missions.".$_POST["lang"].".json", 'w');
 
 fwrite($Handle, preg_replace('/"(\d*?)"([^:])/', '$1$2', str_replace('"true"', "true", json_encode($missions, JSON_PRETTY_PRINT))));
 
