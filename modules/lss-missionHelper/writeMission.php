2cf02afe2d8e0d881875eab9a6120da697f08c8e
Jan <jan.boehmerle@ledbrain.de>
2019-07-30 21:49:15 +0200

    besserer Schutz bei JSON-Fehler

--- modules/lss-missionHelper/writeMission.php
+++ modules/lss-missionHelper/writeMission.php
@@ -5,16 +5,14 @@ $missions[$_POST["id"]] = $_POST["mission"];
 
 ksort($missions);
 
-$Handle = fopen("./missions.".$_POST["lang"].".json", 'w');
-
 json_decode(json_encode($missions, JSON_PRETTY_PRINT), true);
 
-if (json_last_error() === 0) {
-    exit;
+if (json_last_error() !== 0) {
+    $Handle = fopen("./missions." . $_POST["lang"] . ".json", 'w');
+
+    fwrite($Handle, preg_replace('/"(\d*?)"([^:])/', '$1$2', str_replace('"true"', "true", json_encode($missions, JSON_PRETTY_PRINT))));
+
+    fclose($Handle);
+
+    echo "<br>Dieser Einsatz wurde eben live eingetragen! Hier erstmal die Rohdaten:<pre>" . preg_replace('/"(\d*?)"/', '$1', str_replace('"true"', "true", json_encode($_POST["mission"], JSON_PRETTY_PRINT))) . "</pre>";
 }
-
-fwrite($Handle, preg_replace('/"(\d*?)"([^:])/', '$1$2', str_replace('"true"', "true", json_encode($missions, JSON_PRETTY_PRINT))));
-
-fclose($Handle);
-
-echo "<br>Dieser Einsatz wurde eben live eingetragen! Hier erstmal die Rohdaten:<pre>".preg_replace('/"(\d*?)"/', '$1', str_replace('"true"', "true", json_encode($_POST["mission"], JSON_PRETTY_PRINT)))."</pre>";
