9e727fa38f27d65aff15f4a634d2aa99adefadd0
Jan <jan.boehmerle@ledbrain.de>
2019-09-17 19:24:04 +0200

    💄 [destinationFilter] improved Settings Layout
    and some kind of indentation fixes

--- modules/lss-destinationFilter/DestinationFilter.user.js
+++ modules/lss-destinationFilter/DestinationFilter.user.js
@@ -1,48 +1,48 @@
 ((I18n) => {
     const LSS_DESTFILTER_STORAGE = 'LSS_DESTFILTER_STORAGE';
 
-	I18n.translations.de.lssm.destfilter = {
-		title: 'Zielort Filter',
+    I18n.translations.de.lssm.destfilter = {
+        title: 'Zielort Filter',
         freeBeds: 'Freie Betten',
-		tax: 'Abgabe',
-		cellTax: 'Abgabe an Besitzer',
+        tax: 'Abgabe',
+        cellTax: 'Abgabe an Besitzer',
         settings: {
             beds: 'Volle KH ausblenden',
             department: 'KH ohne korrekte Fachabteilung ausblenden',
             distance: 'Ziele über x km Entfernung ausblenden (0 deaktiviert)',
-			tax: 'Ziele mit einer Abgabe von mehr als x% ausblenden',
-			cells: 'Volle Polizeistationen/Zellen ausblenden'
+            tax: 'Ziele mit einer Abgabe von mehr als x% ausblenden',
+            cells: 'Volle Polizeistationen/Zellen ausblenden'
         }
-	};
-	I18n.translations.en.lssm.destfilter = {
-		title: 'Destination Filter',
+    };
+    I18n.translations.en.lssm.destfilter = {
+        title: 'Destination Filter',
         freeBeds: 'Free beds',
-		tax: 'TAX',
-		cellTax: 'owner\'s tax',
+        tax: 'TAX',
+        cellTax: 'owner\'s tax',
         settings: {
             beds: 'Hide full hospitals',
             department: 'Hide hospitals without needed department',
             distance: 'Hide destinations above x km distance (0 deactivates)',
-			tax: 'Hide destinations with a TAX higher than x%',
-			cells: 'Hide full cells'
+            tax: 'Hide destinations with a TAX higher than x%',
+            cells: 'Hide full cells'
         }
-	};
-	I18n.translations.nl.lssm.destfilter = {
-		title: 'Bestemming Filter',
+    };
+    I18n.translations.nl.lssm.destfilter = {
+        title: 'Bestemming Filter',
         freeBeds: 'Vrije bedden',
-		tax: 'Kosten',
-		cellTax: 'Afdrachtpercentage',
+        tax: 'Kosten',
+        cellTax: 'Afdrachtpercentage',
         settings: {
             beds: 'Verberg volledig ziekenhuis',
             department: 'Verberg ziekenhuizen zonder benodigde afdeling',
             distance: 'Verberg bestemmingen boven x km afstand (0 wordt gedeactiveerd)',
-			tax: 'Verberg bestemmingen met een Kosten hoger dan x%.',
-			cells: 'Verberg volle cellen'
+            tax: 'Verberg bestemmingen met een Kosten hoger dan x%.',
+            cells: 'Verberg volle cellen'
         }
-	};
-	
+    };
 
-	const managedSettings = {
+
+    const managedSettings = {
         id: LSS_DESTFILTER_STORAGE,
         title: I18n.t('lssm.destfilter.title'),
         settings: {
@@ -60,6 +60,13 @@
                     type: 'checkbox'
                 }
             },
+            cells: {
+                default: true,
+                ui: {
+                    label: I18n.t('lssm.destfilter.settings.cells'),
+                    type: 'checkbox'
+                }
+            },
             distance: {
                 default: 0,
                 ui: {
@@ -68,23 +75,16 @@
                     min: 0,
                 }
             },
-			tax: {
-				default: 50,
-				ui: {
-					label: I18n.t('lssm.destfilter.settings.tax'),
+            tax: {
+                default: 50,
+                ui: {
+                    label: I18n.t('lssm.destfilter.settings.tax'),
                     type: 'number',
                     min: 0,
-					max: 50,
-					step: 10
-				}
-			},
-			cells: {
-				default: true,
-				ui: {
-					label: I18n.t('lssm.destfilter.settings.cells'),
-					type: 'checkbox'
-				}
-			}
+                    max: 50,
+                    step: 10
+                }
+            }
         }
     };
 
@@ -104,27 +104,27 @@
                 let distance = parseFloat(info.match(/\d+[.,]\d+ km/)[0].replace(/,/, '.').replace(/[^\d.]/g, ''));
                 let freeBeds = parseInt(info.match(new RegExp(`${I18n.t('lssm.destfilter.freeBeds')}: \\d+`))[0].replace(/\D+/g, ''));
                 let department = info.indexOf('label-success') > -1;
-				let tax = 0;
-				let taxMatch = info.match(new RegExp(`${I18n.t('lssm.destfilter.tax')}: \\d+ %`));
-				if (taxMatch) tax = parseInt(taxMatch[0].replace(/\D+/g, ''));
+                let tax = 0;
+                let taxMatch = info.match(new RegExp(`${I18n.t('lssm.destfilter.tax')}: \\d+ %`));
+                if (taxMatch) tax = parseInt(taxMatch[0].replace(/\D+/g, ''));
                 if (getSetting('distance') > 0) distance > getSetting('distance') && hide();
                 getSetting('beds') && freeBeds === 0 && hide();
                 getSetting('department') && !department && hide();
-				tax > getSetting('tax') && hide();
+                tax > getSetting('tax') && hide();
             });
         } else {
-			document.querySelectorAll('.alert-info a.btn[href*="/gefangener/"]').forEach(el => {
-				const hide = () => el.classList.add('hidden');
-				el.classList.remove('hidden');
-				let distance = parseFloat(el.innerText.match(/\d+[.,]\d+ km/)[0].replace(/,/, '.').replace(/[^\d.]/g, ''));
-				let tax = 0;
-				let taxMatch = el.innerText.match(new RegExp(`${I18n.t('lssm.destfilter.cellTax')}: \\d+%`));
-				if (taxMatch) tax = parseInt(taxMatch[0].replace(/\D+/g, ''));
-				getSetting('cells') && el.classList.contains('btn-danger') && hide();
-				if (getSetting('distance') > 0) distance > getSetting('distance') && hide();
-				tax > getSetting('tax') && hide();
-			});
-		}
+            document.querySelectorAll('.alert-info a.btn[href*="/gefangener/"]').forEach(el => {
+                const hide = () => el.classList.add('hidden');
+                el.classList.remove('hidden');
+                let distance = parseFloat(el.innerText.match(/\d+[.,]\d+ km/)[0].replace(/,/, '.').replace(/[^\d.]/g, ''));
+                let tax = 0;
+                let taxMatch = el.innerText.match(new RegExp(`${I18n.t('lssm.destfilter.cellTax')}: \\d+%`));
+                if (taxMatch) tax = parseInt(taxMatch[0].replace(/\D+/g, ''));
+                getSetting('cells') && el.classList.contains('btn-danger') && hide();
+                if (getSetting('distance') > 0) distance > getSetting('distance') && hide();
+                tax > getSetting('tax') && hide();
+            });
+        }
     };
     const settingNode = (key, type, attributes) => {
         const setting = getSetting(key);
@@ -140,7 +140,7 @@
         inputNode.type = type;
         inputNode.value = setting;
         inputNode.id = id;
-		spanNode.style.paddingRight = '0.5em';
+        spanNode.style.paddingRight = '0.5em';
 
         for (let attribute in attributes) {
             if (!attributes.hasOwnProperty(attribute)) continue;
@@ -151,7 +151,7 @@
 
         if (type === 'checkbox') {
             inputNode.classList.add('onoffswitch-checkbox');
-			inputNode.checked = setting;
+            inputNode.checked = setting;
             let labelNode = document.createElement('label');
             labelNode.classList.add('onoffswitch-label');
             labelNode.htmlFor = id;
@@ -161,8 +161,8 @@
             inputWrapper.appendChild(inputNode);
             inputWrapper.appendChild(labelNode);
         }
-		
-		inputNode.addEventListener('change', e => {
+
+        inputNode.addEventListener('change', e => {
             managedSettings.settings[key].value = type === 'checkbox' ? e.currentTarget.checked : e.currentTarget.value;
             lssm.managedSettings.update(managedSettings);
             filter();
@@ -179,16 +179,16 @@
 
     mode === 'hospital' && settingsNode.appendChild(settingNode('beds', 'checkbox'));
     mode === 'hospital' && settingsNode.appendChild(settingNode('department', 'checkbox'));
-	mode === 'prison' && settingsNode.appendChild(settingNode('cells', 'checkbox'));
+    mode === 'prison' && settingsNode.appendChild(settingNode('cells', 'checkbox'));
     settingsNode.appendChild(settingNode('distance', 'number', {
-		min: 0
-	}));
-	settingsNode.appendChild(settingNode('tax', 'number', {
-		min: 0,
-		max: 50,
-		step: 10
-	}));
+        min: 0
+    }));
+    settingsNode.appendChild(settingNode('tax', 'number', {
+        min: 0,
+        max: 50,
+        step: 10
+    }));
 
     document.querySelector('.alert-info').insertAdjacentElement('afterend', settingsNode);
-	filter();
+    filter();
 })(I18n);
