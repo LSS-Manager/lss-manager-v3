0f7a785e540903ccc6e783902527aa93168a0e63
Tsumiki <mirkoiz@hotmail.de>
2019-10-24 15:22:45 +0200

    fixup! fixup! More usage of fallbacks
    I18n is fun....
    
    (cherry picked from commit 330bfef)

--- lss-manager-v3/lss-manager-v3.dev.js
+++ lss-manager-v3/lss-manager-v3.dev.js
@@ -75,11 +75,11 @@ I18n.defaultLocale = 'de';
 I18n.fallbacks = true;
 I18n.locales.de = ['de', 'en', 'eu', 'fj'];
 I18n.locales.nl = ['nl', 'en', 'eu', 'fj', 'de'];
-I18n.locales.es = ['en', 'eu', 'fj', 'de'];
+I18n.locales.es = ['es', 'en', 'eu', 'fj', 'de'];
 // en, fj and eu are essentially the same.
-I18n.locales.en = ['eu', 'fj', 'de'];
-I18n.locales.fj = ['en', 'eu', 'de'];
-I18n.locales.eu = ['en', 'fj', 'de'];
+I18n.locales.en = ['en', 'eu', 'fj', 'de'];
+I18n.locales.fj = ['fj', 'en', 'eu', 'de'];
+I18n.locales.eu = ['eu', 'en', 'fj', 'de'];
 
 if (!I18n.translations.hasOwnProperty('de')) I18n.translations.de = {};
 if (!I18n.translations.hasOwnProperty('en')) I18n.translations.en = {};
