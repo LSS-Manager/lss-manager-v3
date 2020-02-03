f49c199c2bd7afe3ca004d13ddd31a0d783fa640
Tobias <Suchty112@users.noreply.github.com>
2020-02-03 10:36:53 +0100

    :bug: [Core] Fix PT Domain

--- modules/lss-missionKeyword/missionKeyword.user.js
+++ modules/lss-missionKeyword/missionKeyword.user.js
@@ -1333,7 +1333,7 @@
     I18n.translations.pt_PT.lssm.missionKeywordDefaults = {
       name: 'Palavras-chave da missão',
       settingsName: 'Palavras-chave da missão - configurações',
-      domain: 'operador112.pt',
+      domain: 'operador112.com',
       fw: 'FIRE',
       thl: 'TECH',
       abc: 'CBRN',
