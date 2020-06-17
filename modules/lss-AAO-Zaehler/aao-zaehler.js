ece7a1a66f25a9ff96693e09fb01b2508d394859
Tobias <Suchty112@users.noreply.github.com>
2020-06-17 20:44:39 +0200

    ✨ [aaoZaehler] border as in #108
    Resetbutton moved in Heatmap for fix when new cars added is not functionale
    
    Co-Authored-By: Jan <kboe2@users.noreply.github.com>

--- modules/lss-AAO-Zaehler/aao-zaehler.js
+++ modules/lss-AAO-Zaehler/aao-zaehler.js
@@ -3,17 +3,109 @@
  */
 (function ($, I18n) {
     const SETTINGS = "LSS_AAO_ZAEHLER_STORAGE";
-
+    I18n.translations.de_DE['lssm']['aaozaehler'] = {
+        reset: "Reset AAO-Zähler",
+        label1: 'Auf Klick umranden',
+        description1: 'Umrandet eine AAO sobald sie einmal angeklickt wurde.'
+    };
+    I18n.translations.en_US['lssm']['aaozaehler'] = {
+        reset: "Reset AAO counter",
+        label1: 'Border on click',
+        description1: 'Borders an AAO once it has been clicked once.'
+    };
+    I18n.translations.cs_CZ['lssm']['aaozaehler'] = {
+        reset: "Resetujte čítač AAO",
+        label1: 'Hranice po kliknutí',
+        description1: 'Okraje AAO, jakmile na něj jednou kliknete.'
+    };
+    I18n.translations.pl_PL['lssm']['aaozaehler'] = {
+        reset: "Zresetuj licznik AAO",
+        label1: 'Granica na kliknięcie',
+        description1: 'Graniczy z AAO po jednokrotnym kliknięciu.'
+    };
+    I18n.translations.sv_SE['lssm']['aaozaehler'] = {
+        reset: "Återställ AAO-räknaren",
+        label1: 'Gränsa vid klick',
+        description1: 'Gränsar till en AAO när den har klickats en gång.'
+    };
+    I18n.translations.da_DK['lssm']['aaozaehler'] = {
+        reset: "Nulstil AAO-tæller",
+        label1: 'Kant på klik',
+        description1: 'Afgrænser en AAO, når den er blevet klikket én gang.'
+    };
+    I18n.translations.nb_NO['lssm']['aaozaehler'] = {
+        reset: "Tilbakestill AAO-telleren",
+        label1: 'Kant på klikk',
+        description1: 'Grenser til en AAO når den har blitt klikket en gang.'
+    };
+    I18n.translations.it_IT['lssm']['aaozaehler'] = {
+        reset: "Ripristina contatore AAO",
+        label1: 'Confine su click',
+        description1: 'Confina un AAO una volta che è stato cliccato una volta.'
+    };
+    I18n.translations.tr_TR['lssm']['aaozaehler'] = {
+        reset: "AAO sayacını sıfırla"
+    };
+    I18n.translations.fr_FR['lssm']['aaozaehler'] = {
+        reset: "Réinitialiser le compteur AAO",
+        label1: 'Frontière sur clic',
+        description1: "Il suffit de cliquer une fois sur un AAO pour l'ouvrir."
+    };
+    I18n.translations.ru_RU['lssm']['aaozaehler'] = {
+        reset: "Сброс счетчика AAO",
+        label1: 'Граница щелчком мыши',
+        description1: 'Граничит с AAO после однократного нажатия.'
+    };
+    I18n.translations.uk_UA['lssm']['aaozaehler'] = {
+        reset: "Скинути лічильник AAO",
+        label1: 'Межі при натисканні',
+        description1: 'Межує з AAO після того, як його було натиснено один раз.'
+    };
+    I18n.translations.es_ES['lssm']['aaozaehler'] = {
+        reset: "Restablecer contador AAO",
+        label1: 'La frontera en el clic',
+        description1: 'Bordea un AAO una vez que se ha pulsado una vez.'
+    };
+    I18n.translations.pt_PT['lssm']['aaozaehler'] = {
+        reset: "Redefinir contador AAO",
+        label1: 'Fronteira em clique',
+        description1: 'Faz fronteira com uma AAO depois de clicada uma vez.'
+    };
+    I18n.translations.ja_JP['lssm']['aaozaehler'] = {
+        reset: "AAOカウンターをリセット",
+        label1: 'クリックでボーダー',
+        description1: '1回クリックするとAAOの境界線が表示されます。'
+    };
+    I18n.translations.ko_KR['lssm']['aaozaehler'] = {
+        reset: "AAO 카운터 재설정",
+        label1: '클릭시 테두리',
+        description1: '한 번 클릭하면 AAO와 경계를 이룹니다.'
+    };
+    I18n.translations.ro_RO['lssm']['aaozaehler'] = {
+        reset: "Resetați contorul AAO",
+        label1: 'Faceți clic pe clic',
+        description1: 'Încadrează un AAO odată ce a fost făcut clic pe o dată.'
+    };
+    I18n.translations.fi_FI['lssm']['aaozaehler'] = {
+        reset: "Nollaa AAO-laskuri",
+        label1: 'Reuna napsautuksella',
+        description1: 'Rajaa AAO: ta, kun sitä on napsautettu kerran.'
+    };
+    I18n.translations.nl_NL['lssm']['aaozaehler'] = { //AUR
+        reset: "AUR-teller resetten",
+        label1: 'Grens op klik',
+        description1: 'Grenst aan een AUR als er eenmaal op is geklikt.'
+    };
     const managedSettings = {
-        "id": SETTINGS,
-        "title": I18n.t('lssm.apps.aaoZaehler.name'),
-        "settings": {
-            "border": {
-                "default": false,
-                "ui": {
-                    "label": 'Auf Klick umranden',
-                    "type": "toggle",
-                    "description": 'Umrandet eine AAO sobald sie einmal angeklickt wurde.'
+        id: SETTINGS,
+        title: I18n.t('lssm.apps.aaoZaehler.name'),
+        settings: {
+            border: {
+                default: false,
+                ui: {
+                    label: I18n.t('lssm.aaozaehler.label1'),
+                    type: "toggle",
+                    description: I18n.t('lssm.aaozaehler.description1'),
                 }
             },
         }
@@ -25,63 +117,7 @@
      */
     if(!window.location.href.match(/missions\/\d+$/)) return;
 
-    I18n.translations.de_DE['lssm']['aaozaehler'] = {
-        reset: "Reset AAO-Zähler"
-    };
-    I18n.translations.en_US['lssm']['aaozaehler'] = {
-        reset: "Reset AAO counter"
-    };
-    I18n.translations.cs_CZ['lssm']['aaozaehler'] = {
-        reset: "Resetujte čítač AAO"
-    };
-    I18n.translations.pl_PL['lssm']['aaozaehler'] = {
-        reset: "Zresetuj licznik AAO"
-    };
-    I18n.translations.sv_SE['lssm']['aaozaehler'] = {
-        reset: "Återställ AAO-räknaren"
-    };
-    I18n.translations.da_DK['lssm']['aaozaehler'] = {
-        reset: "Nulstil AAO-tæller"
-    };
-    I18n.translations.nb_NO['lssm']['aaozaehler'] = {
-        reset: "Tilbakestill AAO-telleren"
-    };
-    I18n.translations.it_IT['lssm']['aaozaehler'] = {
-        reset: "Ripristina contatore AAO"
-    };
-    I18n.translations.tr_TR['lssm']['aaozaehler'] = {
-        reset: "AAO sayacını sıfırla"
-    };
-    I18n.translations.fr_FR['lssm']['aaozaehler'] = {
-        reset: "Réinitialiser le compteur AAO"
-    };
-    I18n.translations.ru_RU['lssm']['aaozaehler'] = {
-        reset: "Сброс счетчика AAO"
-    };
-    I18n.translations.uk_UA['lssm']['aaozaehler'] = {
-        reset: "Скинути лічильник AAO"
-    };
-    I18n.translations.es_ES['lssm']['aaozaehler'] = {
-        reset: "Restablecer contador AAO"
-    };
-    I18n.translations.pt_PT['lssm']['aaozaehler'] = {
-        reset: "Redefinir contador AAO"
-    };
-    I18n.translations.ja_JP['lssm']['aaozaehler'] = {
-        reset: "AAOカウンターをリセット"
-    };
-    I18n.translations.ko_KR['lssm']['aaozaehler'] = {
-        reset: "AAO 카운터 재설정"
-    };
-    I18n.translations.ro_RO['lssm']['aaozaehler'] = {
-        reset: "Resetați contorul AAO"
-    };
-    I18n.translations.fi_FI['lssm']['aaozaehler'] = {
-        reset: "Nollaa AAO-laskuri"
-    };
-    I18n.translations.nl_NL['lssm']['aaozaehler'] = { //AUR
-        reset: "AUR-teller resetten"
-    };
+    
 
     const getSetting = (setting) => lssm.managedSettings.getSetting(SETTINGS, setting);
 
