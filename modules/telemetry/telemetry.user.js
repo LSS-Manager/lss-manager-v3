4ab27bbbc5bf4af7190473375576d35937177c47
Jan <jan.boehmerle@ledbrain.de>
2020-10-06 21:40:14 +0200

    ♻️💩🍻 [telemetry] add V4-Beta hint
    Please, don't judge anyone for this bad code, just a quick note to gain some few more Beta-Testers

--- modules/telemetry/telemetry.user.js
+++ modules/telemetry/telemetry.user.js
@@ -1,69 +1,217 @@
 (function (I18n, $) {
-	I18n.translations.de_DE.lssm.telemetry = {
-		question: "Der LSS-Manager sendet Meta-Daten an die Entwickler zur Verbesserung des Skriptes und zum finden von Fehlern.\r\n"+
-		"Zu diesen Daten gehören: Deine ID, dein Nutzername, die Anzahl der Wachen, der genutzte Browser und aktivierte Module.\r\n\r\n"+
-		"Stimmst zu diesem zu?"
-	};
+    I18n.translations.de_DE.lssm.telemetry = {
+        question: "Der LSS-Manager sendet Nicht-Personenbezogene Daten an die Entwickler zur Verbesserung des Skriptes und zum finden von Fehlern.\r\n"+
+          "Zu diesen Daten gehören: Deine ID, dein Nutzername, die Anzahl der Wachen, der genutzte Browser und aktivierte Module.\r\n\r\n"+
+          "Stimmst zu diesem zu?",
+        v4: {
+            title: "Beta-Tester für die V.4 gesucht!",
+            content_till_beta: "Liebe/r " + window.user_name + ",<br>wie du vielleicht mitbekommen hast, neigt sich diese Version des LSS-Manager dem Ende entgegen. Eine brandneue Version 4 wird diese hier immer weiter ablösen. Dafür suchen wir aber noch Beta-Tester. Bis einschließlich 20. Oktober kannst du dich unter",
+            content_till_discord: "als Beta-Tester bewerben. Sollten wir dich annehmen, so werden wir dich per Privat-Nachricht informieren. Um die Zahl der Kommunikationswege gering zu halten musst du einen Account im Forum haben oder auf unserem Discord",
+            content_till_thread: "sein. Bei Fragen hierzu kannst du uns jederzeit im Spiel (jxn_30, Ron31 oder SanniHameln) oder im Forum bzw. auf Discord anschreiben. Schau auch gerne mal in unserem Thread im Forum vorbei:",
+            content_end: "<br><br>Viele Grüße,<br>Dein LSSM-Team!",
+            close: "Diese Nachricht schließen."
+        }
+    };
     I18n.translations.en_US.lssm.telemetry = {
-		question: "The LSS Manager sends meta-data to the developers to improve the script and find errors.\r\n" +
-		"These data include: your ID, username, the number of guards, the browser used, and enabled modules.\r\n\r\n" +
-		"Do you agree with this?"
-	};
+        question: "The LSS Manager sends non-personal data to the developers to improve the script and find errors.\r\n" +
+          "These data include: your ID, username, the number of guards, the browser used, and enabled modules.\r\n\r\n" +
+          "Do you agree with this?",
+        v4: {
+            title: "Searching Beta-Testers for LSS-Manager V.4",
+            content_till_beta: "Dear " + window.user_name + ",<br>as you may have heard on, this version of LSS-Manager will come to an end soon. A new Version 4 will replace this one more and more. Before our initial release, we want YOU as a Beta-Tester. Until 20th of October you may apply at ",
+            content_till_discord: "for Beta-Tests. Accepted Beta-Testers will be informed on 24th of October. To limit communication ways, you need either an Account in German Forum or on our Discord: ",
+            content_till_thread: ". If you got any questions, feel free to Ping or DM a @LSSM-Dev on our Discord! You may also have a look at our german Forum Thread:",
+            content_end: "<br><br>Greetings,<br>Yours LSSM-Team!",
+            close: "Close this message."
+        }
+    };
     I18n.translations.pt_PT.lssm.telemetry = {
         question: "O LSS Manager envia dados não pessoais aos desenvolvedores para melhorar o script e encontrar erros.\r\n" +
-            "Esses dados incluem: seu ID, nome de usuário, número de guardas, navegador usado e módulos ativados.\r\n\r\n" +
-            "Você concorda com isso?"
+          "Esses dados incluem: seu ID, nome de usuário, número de guardas, navegador usado e módulos ativados.\r\n\r\n" +
+          "Você concorda com isso?"
     };
     I18n.translations.cs_CZ.lssm.telemetry = {
         question: "Správce LSS odesílá vývojářům jiné než osobní údaje, aby vylepšil skript a zjistil chyby.\r\n" +
-            "Tyto údaje zahrnují: vaše ID, uživatelské jméno, počet strážců, použitý prohlížeč a povolené moduly.\r\n\r\n" +
-            "Souhlasíte s tím?"
+          "Tyto údaje zahrnují: vaše ID, uživatelské jméno, počet strážců, použitý prohlížeč a povolené moduly.\r\n\r\n" +
+          "Souhlasíte s tím?"
     };
     I18n.translations.pl_PL.lssm.telemetry = {
         question: "LSS Manager wysyła dane nieosobowe do programistów w celu ulepszenia skryptu i znalezienia błędów.\r\n" +
-		"Dane te obejmują: Twój identyfikator, nazwę użytkownika, liczbę strażników, używaną przeglądarkę i włączone moduły.\r\n\r\n" +
-		"Zgadzasz się z tym?"
+          "Dane te obejmują: Twój identyfikator, nazwę użytkownika, liczbę strażników, używaną przeglądarkę i włączone moduły.\r\n\r\n" +
+          "Zgadzasz się z tym?"
     };
     I18n.translations.sv_SE.lssm.telemetry = {
         question: "LSS Manager skickar icke-personlig information till utvecklarna för att förbättra skriptet och hitta fel.\r\n" +
-            "Dessa data inkluderar: ditt ID, användarnamn, antalet vakter, den webbläsare som används och aktiverade moduler.\r\n\r\n" +
-            "Håller du med om detta?"
+          "Dessa data inkluderar: ditt ID, användarnamn, antalet vakter, den webbläsare som används och aktiverade moduler.\r\n\r\n" +
+          "Håller du med om detta?"
     };
     I18n.translations.da_DK.lssm.telemetry = {
         question: "LSS Manager sender ikke-personlige data til udviklerne for at forbedre scriptet og finde fejl.\r\n" +
-            "Disse data inkluderer: dit ID, brugernavn, antallet af vagter, den anvendte browser og aktiverede moduler.\r\n\r\n" +
-            "Er du enig i dette?"
+          "Disse data inkluderer: dit ID, brugernavn, antallet af vagter, den anvendte browser og aktiverede moduler.\r\n\r\n" +
+          "Er du enig i dette?"
     };
     I18n.translations.nb_NO.lssm.telemetry = {
         question: "LSS Manager sender ikke-personlige data til utviklerne for å forbedre skriptet og finne feil.\r\n" +
-            "Disse dataene inkluderer: IDen din, brukernavnet, antall vakter, nettleseren som er brukt og aktiverte moduler.\r\n\r\n" +
-            "Er du enig i dette?"
+          "Disse dataene inkluderer: IDen din, brukernavnet, antall vakter, nettleseren som er brukt og aktiverte moduler.\r\n\r\n" +
+          "Er du enig i dette?"
     };
     I18n.translations.it_IT.lssm.telemetry = {
         question: "LSS Manager invia dati non personali agli sviluppatori per migliorare lo script e trovare gli errori.\r\n" +
-            "Questi dati includono: il tuo ID, il tuo nome utente, il numero di guardie, il browser utilizzato e i moduli abilitati.\r\n\r\n" +
-            "Sei d'accordo con questo?"
+          "Questi dati includono: il tuo ID, il tuo nome utente, il numero di guardie, il browser utilizzato e i moduli abilitati.\r\n\r\n" +
+          "Sei d'accordo con questo?"
     };
     I18n.translations.fr_FR.lssm.telemetry = {
         question: "Le gestionnaire LSS envoie des données non personnelles aux développeurs pour améliorer le script et trouver des erreurs.\r\n" +
-            "Ces données comprennent : votre identifiant, votre nom d'utilisateur, le nombre de gardes, le navigateur utilisé et les modules activés.\r\n\r\n" +
-            "Êtes-vous d'accord avec cela ?"
+          "Ces données comprennent : votre identifiant, votre nom d'utilisateur, le nombre de gardes, le navigateur utilisé et les modules activés.\r\n\r\n" +
+          "Êtes-vous d'accord avec cela ?"
     };
     I18n.translations.ko_KR.lssm.telemetry = {
         question: "LSS Manager는 비 개인 데이터를 개발자에게 보내서 스크립트를 개선하고 오류를 찾습니다.\r\n" +
-            "이러한 데이터에는 ID, 사용자 이름, 가드 수, 사용 된 브라우저 및 활성화 된 모듈이 포함됩니다.\r\n\r\n" +
-            "이것에 동의하십니까?"
+          "이러한 데이터에는 ID, 사용자 이름, 가드 수, 사용 된 브라우저 및 활성화 된 모듈이 포함됩니다.\r\n\r\n" +
+          "이것에 동의하십니까?"
     };
     I18n.translations.ro_RO.lssm.telemetry = {
         question: "LSS Manager trimite date non-personale dezvoltatorilor pentru a îmbunătăți script-ul și pentru a găsi erori.\r\n" +
-            "Aceste date includ: ID-ul dvs., numele de utilizator, numărul de gărzi, browserul utilizat și modulele activate.\r\n\r\n" +
-            "Ești de acord cu asta?"
+          "Aceste date includ: ID-ul dvs., numele de utilizator, numărul de gărzi, browserul utilizat și modulele activate.\r\n\r\n" +
+          "Ești de acord cu asta?"
     };
-	I18n.translations.nl_NL.lssm.telemetry = {
-		question: "De LSS Manager stuurt niet-persoonlijke gegevens naar de ontwikkelaars om het script te verbeteren en fouten te vinden.\r\n" +
-			"Deze gegevens omvatten: uw ID, gebruikersnaam, het aantal posten, de gebruikte browser en ingeschakelde modules.\r\n\r\n" +
-			"Bent u het hiermee eens?"
-	};
+    I18n.translations.nl_NL.lssm.telemetry = {
+        question: "De LSS Manager stuurt niet-persoonlijke gegevens naar de ontwikkelaars om het script te verbeteren en fouten te vinden.\r\n" +
+          "Deze gegevens omvatten: uw ID, gebruikersnaam, het aantal bewakers, de gebruikte browser en ingeschakelde modules.\r\n\r\n" +
+          "Bent u het hiermee eens?",
+        v4: {
+            title: "Zoekende Beta-Testers voor LSS-Manager V.4",
+            content_till_beta: "Beste " + window.user_name + ",<br>zoals u wellicht heeft gehoord, zal deze versie van LSS-Manager binnenkort aflopen. Een nieuwe versie 4 zal deze steeds meer vervangen. Voor onze eerste release willen we U als Beta-Tester. Tot 20 oktober kunt u zich aanmelden bij ",
+            content_till_discord: "voor Beta-Tests. Geaccepteerde Beta-Testers zullen op 24 oktober worden geinformeerd. Om de communicatiemogelijkheden te beperken, heeft u een account nodig in het Duitse Forum of op onze Discord: ",
+            content_till_thread: ". Als je vragen hebt, voel je vrij om Ping of DM een @LSSM-Dev op onze Discord! U kunt ook een kijkje nemen op onze Duitse Forum Thread:",
+            content_end: "<br><br>Groeten,<br>Uw LSSM-Team!",
+            close: "Sluit dit bericht"
+        }
+    };
+
+    const V4_BETA_STORAGE = 'LSSM_V4_Beta_Hint';
+
+    if (window.location.pathname === '/' && !localStorage[V4_BETA_STORAGE]) {
+        document.body.innerHTML += `<style>
+.lssm-modal-container {
+  z-index: 5001 !important;
+}
+.lssm-modal-container .lssm-modal-overlay[data-modal=dialog] {
+  background-color: rgba(0, 0, 0, 0.7);
+  pointer-events: none;
+}
+.lssm-modal-container .lssm-modal-modal {
+  padding: 1rem;
+  overflow: auto !important;
+  max-height: 100vh !important;
+}
+
+.lssm-modal-block-scroll {
+  overflow: hidden;
+  width: 100vw;
+}
+.lssm-modal-container {
+  position: fixed;
+  box-sizing: border-box;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  z-index: 999;
+}
+.lssm-modal-overlay {
+  position: fixed;
+  box-sizing: border-box;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100vh;
+  background: rgba(0, 0, 0, 0.2);
+  /* z-index: 999; */
+  opacity: 1;
+}
+.lssm-modal-container.scrollable {
+  height: 100%;
+  min-height: 100vh;
+  overflow-y: auto;
+  -webkit-overflow-scrolling: touch;
+}
+.lssm-modal-modal {
+  position: relative;
+  overflow: hidden;
+  box-sizing: border-box;
+
+  background-color: white;
+  border-radius: 3px;
+  box-shadow: 0 20px 60px -2px rgba(27, 33, 58, 0.4);
+}
+.lssm-modal-container.scrollable .lssm-modal-modal {
+  margin-bottom: 2px;
+}
+.lssm-modal-top-right-slot {
+  display: block;
+  position: absolute;
+  right: 0;
+  top: 0;
+}
+
+.vue-dialog div {
+  box-sizing: border-box;
+}
+.vue-dialog .dialog-flex {
+  width: 100%;
+  height: 100%;
+}
+.vue-dialog .dialog-content {
+  flex: 1 0 auto;
+  width: 100%;
+  padding: 15px;
+  font-size: 14px;
+}
+.vue-dialog .dialog-c-title {
+  font-weight: 600;
+  padding-bottom: 15px;
+}
+.vue-dialog .vue-dialog-buttons {
+  display: flex;
+  flex: 0 1 auto;
+  width: 100%;
+  border-top: 1px solid #eee;
+}
+.vue-dialog .vue-dialog-buttons-none {
+  width: 100%;
+  padding-bottom: 15px;
+}
+.vue-dialog-button {
+  font-size: 12px !important;
+  background: transparent;
+  padding: 0;
+  margin: 0;
+  border: 0;
+  cursor: pointer;
+  box-sizing: border-box;
+  line-height: 40px;
+  height: 40px;
+  color: inherit;
+  font: inherit;
+  outline: none;
+}
+.vue-dialog-button:hover {
+  background: rgba(0, 0, 0, 0.01);
+}
+.vue-dialog-button:active {
+  background: rgba(0, 0, 0, 0.025);
+}
+.vue-dialog-button:not(:first-of-type) {
+  border-left: 1px solid #eee;
+}
+body.dark .lssm-modal-modal {
+  background-color: #505050;
+  color: white;
+}
+</style><div class="lssm-modal-container" id="lssm-v4-modal-container"><div data-modal="dialog" aria-expanded="true" class="lssm-modal-overlay"><div class="lssm-modal-top-right-slot"></div></div> <div aria-expanded="true" role="dialog" aria-modal="true" class="lssm-modal-modal v--modal vue-dialog" style="top: 119px; left: 755px; width: 400px; height: auto;"><div class="dialog-content"><div class="dialog-c-title">${I18n.t('lssm.telemetry.v4.title')}</div> <div class="dialog-c-text">${I18n.t('lssm.telemetry.v4.content_till_beta')} <a class="lightbox-open" href="https://beta.lss-manager.de">https://beta.lss-manager.de</a> ${I18n.t('lssm.telemetry.v4.content_till_discord')} <a class="lightbox-open" href="https://discord.gg/RcTNjpB" target="_blank" >https://discord.gg/RcTNjpB</a> ${I18n.t('lssm.telemetry.v4.content_till_thread')} <a class="lightbox-open" target="_blank" href="https://forum.leitstellenspiel.de/index.php?thread/19176-lss-manager-v-4/">https://forum.leitstellenspiel.de/index.php?thread/19176-lss-manager-v-4/</a> ${I18n.t('lssm.telemetry.v4.content_end')} </div></div> <div class="vue-dialog-buttons"><button type="button" class="vue-dialog-button" style="flex: 1 1 100%;" id="lssm-v4-modal-close">${I18n.t('lssm.telemetry.v4.close')}</button></div> <!----></div></div>`;
+        $('#lssm-v4-modal-close').click(() => {localStorage[V4_BETA_STORAGE] = 'clicked'; $('#lssm-v4-modal-container').remove();});
+    }
 
     function getModules()
     {
