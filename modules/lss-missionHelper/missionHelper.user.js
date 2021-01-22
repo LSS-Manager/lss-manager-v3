dd66d9b9a9884a1c55c207dc1f692d26f6abdbf5
Tobias <Suchty112@users.noreply.github.com>
2021-01-22 13:01:25 +0100

    ✨[Core] New CP's CZ and DK
    DE Mission today
    GB and NL Mission fix

--- modules/lss-missionHelper/missionHelper.user.js
+++ modules/lss-missionHelper/missionHelper.user.js
@@ -713,6 +713,11 @@ const lssm_missionhelper_adjustPosition = () => {
                     (content.innerHTML += `<br>1x ${I18n.t(
                         'lssm.missionhelper.requirements.orgl'
                     )} (100%)`);
+                I18n.locale === 'da_DK' &&
+                    patients >= 10 &&
+                    (content.innerHTML += `<br>1x ${I18n.t(
+                        'lssm.missionhelper.requirements.orgl'
+                    )} (100%)`);
                 I18n.locale === 'en_US' &&
                     patients >= 10 &&
                     (content.innerHTML += `<br>1x ${I18n.t(
