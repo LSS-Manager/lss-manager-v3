338809fc761e23a3233a008dca711f3aa3dc2d3d
jan <jan.boehmerle@ledbrain.de>
2022-11-18 16:17:00 +0100

    ♻️ add fi_FI url in userscript

--- lssm-v3.user.js
+++ lssm-v3.user.js
@@ -1,44 +1,37 @@
 // ==UserScript==
 // @name         LSS Manager v3 Autoupdater
-// @version      3.3.9
+// @version      3.3.10
 // @namespace    https://lss-manager.de/
 // @description  Das Tool für LSS
 // @author       lost | northdegree | mausmajor | dlrg-dominik | mrweezle | sanni | jan
 // @homepage     https://lss-manager.de
-// @include      *://leitstellenspiel.de/*
-// @include      *://www.leitstellenspiel.de/*
-// @include      *://missionchief.co.uk/*
-// @include      *://www.missionchief.co.uk/*
-// @include      *://missionchief.com/*
-// @include      *://www.missionchief.com/*
-// @include      *://meldkamerspel.com/*
-// @include      *://www.meldkamerspel.com/*
-// @include      *://centro-de-mando.es/*
-// @include      *://www.centro-de-mando.es/*
-// @include      *://missionchief-australia.com/*
-// @include      *://www.missionchief-australia.com/*
-// @include      *://larmcentralen-spelet.se/*
-// @include      *://www.larmcentralen-spelet.se/*
-// @include      *://alarmcentral-spil.dk/*
-// @include      *://www.alarmcentral-spil.dk/*
-// @include      *://operatorratunkowy.pl/*
-// @include      *://www.operatorratunkowy.pl/*
-// @include      *://operatore112.it/*
-// @include      *://www.operatore112.it/*
-// @include      *://operateur112.fr/*
-// @include      *://www.operateur112.fr/*
-// @include      *://nodsentralspillet.com/*
-// @include      *://www.nodsentralspillet.com/*
-// @include      *://operacni-stredisko.cz/*
-// @include      *://www.operacni-stredisko.cz/*
-// @include      *://jogo-operador112.com/*
-// @include      *://www.jogo-operador112.com/*
-// @include      *://missionchief-korea.com/*
-// @include      *://www.missionchief-korea.com/*
-// @include      *://jocdispecerat112.com/*
-// @include      *://www.jocdispecerat112.com/*
+// @match        https://www.operacni-stredisko.cz/*
+// @match        https://www.alarmcentral-spil.dk/*
+// @match        https://www.leitstellenspiel.de/*
+// @match        https://www.missionchief-australia.com/*
+// @match        https://www.missionchief.co.uk/*
+// @match        https://www.missionchief.com/*
+// @match        https://www.centro-de-mando.es/*
+// @match        https://www.centro-de-mando.mx/*
+// @match        https://www.hatakeskuspeli.com/*
+// @match        https://www.operateur112.fr/*
+// @match        https://www.operatore112.it/*
+// @match        https://www.missionchief-japan.com/*
+// @match        https://www.missionchief-korea.com/*
+// @match        https://www.nodsentralspillet.com/*
+// @match        https://www.meldkamerspel.com/*
+// @match        https://www.operatorratunkowy.pl/*
+// @match        https://www.operador193.com/*
+// @match        https://www.jogo-operador112.com/*
+// @match        https://www.jocdispecerat112.com/*
+// @match        https://www.dispetcher112.ru/*
+// @match        https://www.dispecerske-centrum.com/*
+// @match        https://www.larmcentralen-spelet.se/*
+// @match        https://www.112-merkez.com/*
+// @match        https://www.dyspetcher101-game.com/*
 // @updateURL    https://github.com/LSS-Manager/lss-manager-v3/raw/master/lssm-v3.user.js
+// @downloadURL  https://github.com/LSS-Manager/lss-manager-v3/raw/master/lssm-v3.user.js
 // @grant        none
-// @run          document-start
+// @run-at       document-idle
 // ==/UserScript==
 if("undefined"==typeof jQuery)throw new Error("LSS-Manager: No jQuery! Aborting!");$.ajaxPrefilter(function(e,a,s){("script"==e.dataType||"script"==a.dataType||"stylesheet"==e.dataType||"stylesheet"==a.dataType)&&(e.cache=!0)});var uid="";"undefined"!=typeof user_id&&(game=window.location.hostname.toLowerCase().replace("www.","").split(".")[0],uid="?uid="+game+user_id),$("head").append('<script id="lss_manager_js" src="https://v3.lss-manager.de/lss-manager-v3/lss-manager-v3.dev.js'+uid+'" type="text/javascript"></script>');
